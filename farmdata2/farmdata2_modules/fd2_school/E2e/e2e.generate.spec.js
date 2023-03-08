830266edd854b2ce82474b0228a81b17ac4a3791
infantlikesprogramming <hadu@dickinson.edu>
2023-03-08 09:56:54 -0500

    Create a test for the report additional info after  generate button is clicked

--- farmdata2/farmdata2_modules/fd2_school/E2e/e2e.generate.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/E2e/e2e.generate.spec.js
@@ -4,9 +4,18 @@ describe("Test the Generate Button", ()=>{
         cy.visit("/farm/fd2-school/e2e")
     })
 
-    it("Check generate button",()=>{
+    it("Check generate button - harvest report title",()=>{
         cy.get("[data-cy=generate-title]").should("not.exist")
         cy.get("[data-cy=generate-button]").click()
         cy.get("[data-cy=generate-title]").should("be.visible")
     })
+
+    it("Check generate button - farm info",()=>{
+        
+        cy.get("[data-cy=generate-button]").click()
+        cy.get("[data-cy=farm-name]").should("have.text","Sample Farm")
+        cy.get("[data-cy=farm-user]").should("have.text","manager1")
+        cy.get("[data-cy=farm-language]").should("have.text","English")
+    })
+
 })
