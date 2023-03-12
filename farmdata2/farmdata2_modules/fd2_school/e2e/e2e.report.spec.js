7dafdbe696e639a13d43cc334fc88df5cb2cf4b9
sidlamsal <lamsals@dickinson.edu>
2023-03-12 19:47:22 -0400

    Tested generate report button and report header

---
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.report.spec.js
@@ -0,0 +1,19 @@
+describe("Test the generated harvest report", () => {
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-school/e2e")
+    })
+
+    it("Check harvest report", () => {
+        cy.get("[data-cy=report-title]")
+            .should("not.exist")
+
+        cy.get("[data-cy=generate-report-button]")
+            .click()
+
+        cy.get("[data-cy=report-title]")
+            .should("be.visible")
+    })
+      
+ 
+})
