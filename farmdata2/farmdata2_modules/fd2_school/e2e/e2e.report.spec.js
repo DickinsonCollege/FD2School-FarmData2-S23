499a0c663368d357eac72f9866ae5a7fd40cc365
sidlamsal <lamsals@dickinson.edu>
2023-03-12 20:10:50 -0400

    Tested report details

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.report.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.report.spec.js
@@ -4,7 +4,7 @@ describe("Test the generated harvest report", () => {
         cy.visit("/farm/fd2-school/e2e")
     })
 
-    it("Check harvest report", () => {
+    it("Check report title", () => {
         cy.get("[data-cy=report-title]")
             .should("not.exist")
 
@@ -15,5 +15,19 @@ describe("Test the generated harvest report", () => {
             .should("be.visible")
     })
       
+    it("Check report details", () => {
+        cy.get("[data-cy=generate-report-button]")
+            .click()
+
+        cy.get("[data-cy=farm-name]")
+            .should("have.text", "Farm:Sample Farm")
+
+        cy.get("[data-cy=username]")
+            .should("contain.text", "manager1")
+
+        cy.get("[data-cy=language")
+            .should("have.text", "English")
+
+    })
  
 })
