bbfdb2cbfb2fdd55677cfae36ac600634541c65a
phong260702 <phongdangquoc123@gmail.com>
2023-03-13 01:36:38 -0400

    Add test for checking text about farm information

--- farmdata2/farmdata2_modules/fd2_school/E2E/e2e.interactive.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/E2E/e2e.interactive.spec.js
@@ -10,4 +10,11 @@ describe("Test the harvest report interactive values", () => {
         cy.get("[data-cy=report-title").should("be.visible");
     });
 
+    it("Check Farm Name after click button", () => {
+        cy.get("[data-cy=generate-button]").click();
+        cy.get("[data-cy=farm-name]").should("have.text", "Farm: Sample Farm");
+        cy.get("[data-cy=user-name]").should("contain.text", "manager1");
+        cy.get("[data-cy=language]").should("have.text", "English");
+    });
+
 });
