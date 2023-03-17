1cba6ddd42b5f77ac34f645554ee5c97ed9c7a2b
Shahir-47 <ahmeds@dickinson.edu>
2023-03-17 11:53:52 -0400

    Checks the details of the report page

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.dynamic.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.dynamic.spec.js
@@ -7,8 +7,23 @@ describe("Test the harvest report dynamic values", () => {
     it("Check the generate report button", () => {
         cy.get("[data-cy=report-header]")
             .should("not.exist")
+
         cy.get("[data-cy=generate-button]").click()
+
         cy.get("[data-cy=report-header]")
             .should("be.visible")
+    })
+
+    it("Check specific information about the harvest page when report page is generated", () => {
+        cy.get("[data-cy=generate-button]").click()
+
+        cy.get("[data-cy=farm-name]")
+            .should("have.text","Farm:Sample Farm")
+
+        cy.get("[data-cy=user-name]")
+            .should("contain.text","manager1")
+
+        cy.get("[data-cy=language]")
+            .should("have.text","English")
     })
 })
