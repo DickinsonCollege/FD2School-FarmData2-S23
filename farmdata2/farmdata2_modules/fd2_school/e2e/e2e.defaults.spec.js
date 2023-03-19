9f35593df2b13928c2573eb011602ea6420137b2
nguyenbanhducA1K51 <ducnguyen.wings@gmail.com>
2023-03-19 03:59:56 -0400

    add start crop dropdown test to e2e.defaults.spec.js

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -2,17 +2,25 @@ describe("Test the harvest report default values", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
         cy.visit("/farm/fd2-school/e2e")
-        cy.get("[data-cy=page-header]")
-        .should("have.text"," Harvest Report")
+        
 
     })
       
     it("Check the page header", () => {
+        cy.get("[data-cy=page-header]")
+        .should("have.text"," Harvest Report")
     
     })
     it("Check the start date and end date", () => {
         cy.get("[data-cy=start-date]").should("have.value","2020-05-05")
         cy.get("[data-cy=end-date]").should("have.value","2020-05-15")
+    })
+
+    it("Check the crop dropdown", () => {
+        cy.get("[data-cy=crop-item]").children().eq(0).should("have.text","ARUGULA")
+        cy.get("[data-cy=crop-item]").children().eq(4).should("have.text","BEAN-FAVA")
+        cy.get("[data-cy=crop-item]").children().eq(110).should("have.text","ZUCCHINI")
+        cy.get("[data-cy=crop-item]").children().should("have.length","111")
     })
 
 })
