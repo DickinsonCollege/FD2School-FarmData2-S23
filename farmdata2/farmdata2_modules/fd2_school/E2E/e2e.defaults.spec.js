b2984fb275044da949e823e113852a97be37b812
udvale <enkhtaiu@dickinson.edu>
2023-03-20 23:26:52 -0400

    Updated testing for crop and area dropdown

--- farmdata2/farmdata2_modules/fd2_school/E2E/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/E2E/e2e.defaults.spec.js
@@ -12,9 +12,15 @@ describe("Test the harvest report default values", () => {
         cy.get("[data-cy = end-date]").should("have.value" , "2020-05-15")
     })
     it("Check the crop dropdown", () => {
-        cy.get("[data-cy = crop-dropdown]").children().eq(0).should("eq" , "ARUGULA")
-        cy.get("[data-cy = crop-dropdown]").children().eq(4).should("eq" , "BEAN-FAVA")
-        cy.get("[data-cy = crop-dropdown]").children().eq(110).should("eq" , "ZUCCHINI")
+        cy.get("[data-cy = crop-dropdown]").children().eq(0).should("have.value" , "ARUGULA")
+        cy.get("[data-cy = crop-dropdown]").children().eq(4).should("have.value" , "BEAN-FAVA")
+        cy.get("[data-cy = crop-dropdown]").children().eq(110).should("have.value" , "ZUCCHINI")
         cy.get("[data-cy = crop-dropdown]").children().should("have.length" , 111)
+    })
+    it("Check the area dropdown", () => {
+        cy.get("[data-cy = area-dropdown]").children().eq(0).should("have.value" , "A")
+        cy.get("[data-cy = area-dropdown]").children().eq(4).should("have.value" , "ALF-3")
+        cy.get("[data-cy = area-dropdown]").children().eq(69).should("have.value" , "Z")
+        cy.get("[data-cy = area-dropdown]").children().should("have.length" , 70)
     })
 })
