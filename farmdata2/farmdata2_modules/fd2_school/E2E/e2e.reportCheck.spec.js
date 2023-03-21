42d360260d2311bd81fab7b6e6912a9ec2e8a319
udvale <enkhtaiu@dickinson.edu>
2023-03-20 23:15:28 -0400

    Tested the details after generating report

--- farmdata2/farmdata2_modules/fd2_school/E2E/e2e.reportCheck.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/E2E/e2e.reportCheck.spec.js
@@ -9,7 +9,9 @@ describe("Check Harvest Report when the generate button is clicked", () => {
     it("Get Generate Report button", () => {
         cy.get("[data-cy = generate-report-button]").click()
         cy.get("[data-cy = report-header]").should("be.visible")
-        cy.get("[data-cy = farm-name]").should("have.text", "Sample Farm")
+        cy.get("[data-cy = farm-name]").should("have.text", "Farm: Sample Farm")
+        cy.get("[data-cy = farm-user]").should("contain.text", "manager1")
+        cy.get("[data-cy = farm-language]").should("have.text", "English")
     })
 
 })
