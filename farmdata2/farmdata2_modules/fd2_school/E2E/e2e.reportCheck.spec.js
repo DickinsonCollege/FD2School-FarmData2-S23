8397a8be6e90113c774d02de632ad610e0feab0e
udvale <enkhtaiu@dickinson.edu>
2023-03-20 22:30:54 -0400

    Cypress testing in generating report button and header

---
+++ farmdata2/farmdata2_modules/fd2_school/E2E/e2e.reportCheck.spec.js
@@ -0,0 +1,14 @@
+describe("Check Harvest Report when the generate button is clicked", () => {
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-school/e2e")
+    })
+    it("Details should be not visible", () => {
+        cy.get("[data-cy = report-header]").should("not.exist")
+    })
+    it("Get Generate Report button", () => {
+        cy.get("[data-cy = generate-report-button]").click()
+        cy.get("[data-cy = report-header]").should("be.visible")
+    })
+
+})
