dec743c8effe5ff1e0ef9020c2e076cab099d48b
JinLeeGG <leejo@dickinson.edu>
2023-03-21 01:31:33 -0400

    added new test case that checks crops dropdown

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -15,6 +15,27 @@ describe("Test the harvest report default values", () => {
     cy.get('[data-cy="end-date"]')
       .should('have.value', '2020-05-15')
   })
+
+  it("checks that crop dropdown is correctly working", () => {
+    cy.get('[data-cy="crop-selected"]')
+      .children()
+      .eq(0)
+      .should("have.text", "ARUGULA")
+
+    cy.get('[data-cy="crop-selected"]')
+      .children()
+      .eq(4)
+      .should("have.text", "BEAN-FAVA")
+
+    cy.get('[data-cy="crop-selected"]')
+      .children()
+      .eq(110)
+      .should("have.text", "ZUCCHINI")
+
+    cy.get('[data-cy="crop-selected"]')
+      .children()
+      .should("have.length", 111)
+  })
 })
 
 
