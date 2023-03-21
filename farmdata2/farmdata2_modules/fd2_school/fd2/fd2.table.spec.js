fba8940b929abe6ef4861abac2c7d77a78dee1c5
braught <braught@dickinson.edu>
2023-03-21 11:19:53 -0400

    Tested that table headers are set correctly

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -4,7 +4,12 @@ describe("Test the harvest report table", () => {
         cy.visit("/farm/fd2-school/fd2")
     })
       
-    it("empty", () => {
-
+    it("Check the table headers", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=h0]").should("have.text","Date")
+        cy.get("[data-cy=h1]").should("have.text","Area")
+        cy.get("[data-cy=h2]").should("have.text","Crop")
+        cy.get("[data-cy=h3]").should("have.text","Yield")
+        cy.get("[data-cy=h4]").should("have.text","Units")
     })
 })
