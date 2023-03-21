1cadba11d3e20de2d458743b78f2e167e9a1595e
mikek16 <krausem@dickinson.edu>
2023-03-21 10:03:38 -0400

    Updated Module

---
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -0,0 +1,10 @@
+describe("Test the harvest report default values", () =>{
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-school/e2e")
+
+    })
+    it("Check the page Header", () =>{
+        
+    })
+})
