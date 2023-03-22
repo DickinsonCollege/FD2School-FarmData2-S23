b17cd06fbfb6c1bc94c72bec44fee62cece9d88a
mikek16 <krausem@dickinson.edu>
2023-03-22 12:19:39 -0400

    Harvest report data testing

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.harvest.report.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.harvest.report.spec.js
@@ -9,5 +9,11 @@ describe("Test the Harvest Report Generation", () => {
         cy.get("[data-cy=generate-report]").click()
         cy.get("[data-cy=report-header]").should("be.visible")
     })
+    it("Checks the Harvest Report Details", () => {
+        cy.get("[data-cy=generate-report]").click()
+        cy.get("[data-cy=farm-li]").should("have.text", "Farm: Sample Farm")
+        cy.get("[data-cy=user-li]").should("contain.text", "manager1")
+        cy.get("[data-cy=language-li]").should("have.text","English")
+    })
 
 })
