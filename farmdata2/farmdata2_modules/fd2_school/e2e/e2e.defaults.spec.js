d72f185d7ad143256201d11b225a9fc7ce03a844
pranavm2109 <mishrap@dickinson.edu>
2023-03-22 00:14:28 -0400

    checked contents of the crop dropdown menu

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -16,4 +16,19 @@ describe("Test the harvest report default values", () => {
         cy.get("[data-cy=end-date]")
             .should("have.value", "2020-05-15")
     })
+
+    it("Check the contents of the crop dropdown menu", () => {
+        cy.get("[data-cy=crop-dropdown-menu]").children().eq(0)
+            .should("have.value", "ARUGULA")
+
+        cy.get("[data-cy=crop-dropdown-menu]").children().eq(4)
+            .should("have.value", "BEAN-FAVA")
+
+        cy.get("[data-cy=crop-dropdown-menu]").children().eq(110)
+            .should("have.value", "ZUCCHINI")
+
+        cy.get("[data-cy=crop-dropdown-menu]").children()
+            .should("have.length", "111")
+            
+    })
 })
