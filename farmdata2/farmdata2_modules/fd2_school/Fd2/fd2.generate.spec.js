cd1cfa4fce8dc91c05f38b71bcf8d43d1209e570
infantlikesprogramming <hadu@dickinson.edu>
2023-03-22 09:55:28 -0400

    Modified fd2.defaults.spec.js to test the new dropdown element

--- farmdata2/farmdata2_modules/fd2_school/Fd2/fd2.generate.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/Fd2/fd2.generate.spec.js
@@ -1,7 +1,7 @@
 describe("Test the Generate Button", ()=>{
     beforeEach(()=>{
         cy.login("manager1", "farmdata2")
-        cy.visit("/farm/fd2-school/e2e")
+        cy.visit("/farm/fd2-school/fd2")
     })
 
     it("Check generate button - harvest report title",()=>{
