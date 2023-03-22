c8586d0c17217ed029ae6e9b407749f331e36019
EliasBerhe <eliastes123@gmail.com>
2023-03-21 20:06:36 -0400

    Test Harvest Report Details

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.report.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.report.spec.js
@@ -11,5 +11,13 @@ describe("Test the harvest report Generations", () => {
         cy.get("[data-cy=report-title]").should('be.visible')
        
     })
+
+    it("Check the harvest report generated details", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=farm]").should('have.text', 'Farm: Sample Farm')
+        cy.get("[data-cy=username]").should('contain.text', 'manager1')
+        cy.get("[data-cy=language]").should('have.text', 'English')
+       
+    })
     
 })
