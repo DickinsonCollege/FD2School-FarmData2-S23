c07b0ddadc17ffcb36ca2d37810b327490375fe0
pranavm2109 <mishrap@dickinson.edu>
2023-03-22 01:36:47 -0400

    tested name of farm, username and language in harvest report

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.interactions.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.interactions.spec.js
@@ -14,4 +14,26 @@ describe("Test user interactions in the harvest report", () => {
             .should("be.visible")
     })
 
+    it("Test button to generate report", () => {
+        cy.get("[data-cy=farm-name]")
+            .should("not.exist")
+        
+        cy.get("[data-cy=user-name]")
+            .should("not.exist")
+
+        cy.get("[data-cy=language]")
+            .should("not.exist")
+
+        cy.get("[data-cy=generate-report-button]").click()
+
+        cy.get("[data-cy=farm-name]")
+            .should("have.text", "Farm:Sample Farm")
+
+        cy.get("[data-cy=user-name]")
+            .should("contain.text", "manager1")
+
+        cy.get("[data-cy=language]")
+            .should("have.text", "English")
+    })
+
 })
