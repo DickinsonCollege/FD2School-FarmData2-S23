2500e452201034aa6a5b5ff8144bc9753b5f9d0f
pranavm2109 <mishrap@dickinson.edu>
2023-03-25 11:36:25 -0400

    Added FD2 Tab to FD2-School Tab

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.interactions.spec.js
@@ -0,0 +1,39 @@
+describe("Test user interactions in the harvest report", () => {
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-school/e2e")
+    })
+
+    it("Test button to generate report", () => {
+        cy.get("[data-cy=harvest-report-title]")
+            .should("not.exist")
+
+        cy.get("[data-cy=generate-report-button]").click()
+
+        cy.get("[data-cy=harvest-report-title]")
+            .should("be.visible")
+    })
+
+    it("Test farm name, username and language", () => {
+        cy.get("[data-cy=farm-name]")
+            .should("not.exist")
+
+        cy.get("[data-cy=user-name]")
+            .should("not.exist")
+
+        cy.get("[data-cy=language]")
+            .should("not.exist")
+
+        cy.get("[data-cy=generate-report-button]").click()
+
+        cy.get("[data-cy=farm-name]")
+            .should("have.text", "Farm:Sample Farm")
+
+        cy.get("[data-cy=user-name]")
+            .should("contain.text", "manager1")
+
+        cy.get("[data-cy=language]")
+            .should("have.text", "English")
+    })
+
+})
