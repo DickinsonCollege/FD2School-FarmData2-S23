c8ca270495069cd900e6afd142555287ee3e237b
andrewscheiner53 <scheinea@dickinson.edu>
2023-03-25 14:47:09 -0400

    Changed cypress files to visit FD2 (component spike) tab in beforeEach

--- farmdata2/farmdata2_modules/fd2_school/FD2/fd2.checkReport.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/FD2/fd2.checkReport.spec.js
@@ -1,7 +1,7 @@
 describe("Test values once generate report button clicked", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
-        cy.visit("/farm/fd2-school/e2e")
+        cy.visit("/farm/fd2-school/FD2")
     })
       
     it("Check that the report title does not exist at first", () => {
