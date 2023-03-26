c2ac1b316018acec1697d93f7fb60396e052ce0b
nguyenbanhducA1K51 <ducnguyen.wings@gmail.com>
2023-03-25 23:06:55 -0400

    add table header test

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -0,0 +1,28 @@
+describe("Test the harvest report default values", () => {
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-school/fd2")
+        
+
+    })
+      
+    it("Check the table header", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=h0]")
+        .should("have.text","Date")
+        cy.get("[data-cy=h1]")
+        .should("have.text","Area")
+        cy.get("[data-cy=h2]")
+        .should("have.text","Crop")
+        cy.get("[data-cy=h3]")
+        .should("have.text","yield")
+        cy.get("[data-cy=h4]")
+        .should("have.text","unit")
+
+    
+    })
+    
+
+    
+
+})
