700e2b99f3b98eff32fdf074eea7fcd8e2fe33d6
JinLeeGG <leejo@dickinson.edu>
2023-03-26 02:04:23 -0400

    cypress test cases now passes properly

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -17,24 +17,20 @@ describe("Test the harvest report default values", () => {
   })
 
   it("checks that crop dropdown is correctly working", () => {
-    cy.get('[data-cy="crop-selected"]')
-      .children()
-      .eq(0)
+    cy.get('[data-cy="crop-selected"] > [data-cy=dropdown-input] > [data-cy=option0]')
+      .should("have.text", "All")
+
+    cy.get('[data-cy="crop-selected"] > [data-cy=dropdown-input] > [data-cy=option1]')
       .should("have.text", "ARUGULA")
 
-    cy.get('[data-cy="crop-selected"]')
-      .children()
-      .eq(4)
-      .should("have.text", "BEAN-FAVA")
+    cy.get('[data-cy="crop-selected"] > [data-cy=dropdown-input] > [data-cy=option4]')
+      .should("have.text", "BEAN-DRY")
 
-    cy.get('[data-cy="crop-selected"]')
-      .children()
-      .eq(110)
+    cy.get('[data-cy="crop-selected"] > [data-cy=dropdown-input] > [data-cy=option111]')
       .should("have.text", "ZUCCHINI")
 
-    cy.get('[data-cy="crop-selected"]')
-      .children()
-      .should("have.length", 111)
+    cy.get('[data-cy="crop-selected"] > [data-cy=dropdown-input]').children()
+      .should("have.length", 112)
   })
 })
 
