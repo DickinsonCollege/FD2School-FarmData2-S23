40d364102bb33357dc0b08a8efae9ab7b34e3aa5
nathang15 <nguyenat@dickinson.edu>
2023-03-26 16:18:13 -0400

    Add Custom Table Headers Test

---
+++ farmdata2/farmdata2_modules/fd2_school/FD2/FD2.table.spec.js
@@ -0,0 +1,20 @@
+describe("Test the harvest report default values", () => {
+  beforeEach(() => {
+      cy.login("manager1", "farmdata2")
+      cy.visit("/farm/fd2-school/fd2")
+  })
+
+  it("Check Custom Table Headers", () => {
+    cy.get("[data-cy=generate-report-button]").click()
+    cy.get("[data-cy=h0]")
+    .should("have.text","Date")
+    cy.get("[data-cy=h1]")
+    .should("have.text","Area")
+    cy.get("[data-cy=h2]")
+    .should("have.text","Crop")
+    cy.get("[data-cy=h3]")
+    .should("have.text","Yield")
+    cy.get("[data-cy=h4]")
+    .should("have.text","Unit")
+  })
+})
