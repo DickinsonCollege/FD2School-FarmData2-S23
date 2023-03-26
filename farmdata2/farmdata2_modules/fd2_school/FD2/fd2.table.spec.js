ccca6058d26be219fa5c148c27fe1320dd02b489
phong260702 <phongdangquoc123@gmail.com>
2023-03-26 08:46:19 -0400

    Adding new cypress test file for data Table

---
+++ farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
@@ -0,0 +1,11 @@
+describe("Testing the datafarm2 table", () => {
+  beforeEach(() => {
+    cy.login("manager1", "farmdata2");
+    cy.visit("farm/fd2_school/FD2");
+  });
+  
+  it("Testing", () => {
+
+  });
+  
+});
