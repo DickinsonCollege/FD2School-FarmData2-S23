850bc36f305695a33b6848fb77bcbd34933be0d8
phong260702 <phongdangquoc123@gmail.com>
2023-03-26 09:32:03 -0400

    Adding test checking the number of headers

--- farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
@@ -6,12 +6,14 @@ describe("Testing the datafarm2 table", () => {
   
   it("Check the headers of the table", () => {
     cy.get("[data-cy=generate-button]").click();
-    cy.get("[data-cy=h0]").should("have.text", "ID")
-    cy.get("[data-cy=h1]").should("have.text", "Date")
-    cy.get("[data-cy=h2]").should("have.text", "Area")
-    cy.get("[data-cy=h3]").should("have.text", "Crop")
-    cy.get("[data-cy=h4]").should("have.text", "Yield")
-    cy.get("[data-cy=h5]").should("have.text", "Unit")
+    cy.get("[data-cy=h0]").should("have.text", "ID");
+    cy.get("[data-cy=h1]").should("have.text", "Date");
+    cy.get("[data-cy=h2]").should("have.text", "Area");
+    cy.get("[data-cy=h3]").should("have.text", "Crop");
+    cy.get("[data-cy=h4]").should("have.text", "Yield");
+    cy.get("[data-cy=h5]").should("have.text", "Unit");
+    cy.get("[data-cy=table-headers]").children().should("have.length", 8);
   });
+
   
 });
