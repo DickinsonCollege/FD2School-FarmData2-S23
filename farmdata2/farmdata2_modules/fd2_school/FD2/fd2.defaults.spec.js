40a517b58a5d314b23435a06b194c49c64c5a376
udvale <enkhtaiu@dickinson.edu>
2023-03-27 18:33:09 -0400

    Cypress testing crop dropdown DropdownWithAllComponent

--- farmdata2/farmdata2_modules/fd2_school/FD2/fd2.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/FD2/fd2.defaults.spec.js
@@ -1,7 +1,7 @@
 describe("Test the harvest report default values", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
-        cy.visit("/farm/fd2-school/e2e")
+        cy.visit("/farm/fd2-school/fd2")
     })
     it("Check the page header", () => {
         cy.get("[data-cy = page-header]")
@@ -12,10 +12,11 @@ describe("Test the harvest report default values", () => {
         cy.get("[data-cy = end-date]").should("have.value" , "2020-05-15")
     })
     it("Check the crop dropdown", () => {
-        cy.get("[data-cy = crop-dropdown]").children().eq(0).should("have.value" , "ARUGULA")
-        cy.get("[data-cy = crop-dropdown]").children().eq(4).should("have.value" , "BEAN-FAVA")
-        cy.get("[data-cy = crop-dropdown]").children().eq(110).should("have.value" , "ZUCCHINI")
-        cy.get("[data-cy = crop-dropdown]").children().should("have.length" , 111)
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option0]").should("have.value" , "All")
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option1]").should("have.value" , "ARUGULA")
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option5]").should("have.value" , "BEAN-FAVA")
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option111]").should("have.value" , "ZUCCHINI")
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input]").children().should("have.length" , 112)
     })
     it("Check the area dropdown", () => {
         cy.get("[data-cy = area-dropdown]").children().eq(0).should("have.value" , "A")
--- farmdata2/farmdata2_modules/fd2_school/FD2/fd2.reportCheck.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/FD2/fd2.reportCheck.spec.js
@@ -1,7 +1,7 @@
 describe("Check Harvest Report when the generate button is clicked", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
-        cy.visit("/farm/fd2-school/e2e")
+        cy.visit("/farm/fd2-school/fd2")
     })
     it("Details should be not visible", () => {
         cy.get("[data-cy = report-header]").should("not.exist")
