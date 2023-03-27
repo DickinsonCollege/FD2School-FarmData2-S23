0306ec11bc31876c87e2de272e716faf638dc5d5
JinLeeGG <leejo@dickinson.edu>
2023-03-27 01:28:59 -0400

    fd2.table.spec.js added for testing contents of harvestReport

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -0,0 +1,10 @@
+describe("Test contents of harvest report table", () => {
+  beforeEach(() => {
+      cy.login("manager1", "farmdata2")
+      cy.visit("/farm/fd2-school/FD2")
+  })
+
+  it("", () => {
+
+  })
+})
