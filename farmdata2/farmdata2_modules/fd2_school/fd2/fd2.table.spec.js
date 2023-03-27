75611aae036080e375764635bd09152bf3342bdd
JinLeeGG <leejo@dickinson.edu>
2023-03-27 01:34:35 -0400

    added cypress tests that checks correct number of columns

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -13,4 +13,9 @@ describe("Test contents of harvest report table", () => {
     cy.get("[data-cy=h4]").should("have.text","Yield")
     cy.get("[data-cy=h5]").should("have.text","Unit")
   })
+
+  it("Tests for correct number of columns", () => {
+    cy.get("[data-cy=generate-report-button]").click()
+    cy.get("[data-cy=table-headers]").children().should("have.length", 6)
+})
 })
