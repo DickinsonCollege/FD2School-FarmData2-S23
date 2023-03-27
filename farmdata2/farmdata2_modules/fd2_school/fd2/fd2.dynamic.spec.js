57f2fd7260e2c53238207f54cf194e352124ef20
Alexandrialexie <lexielb28@gmail.com>
2023-03-26 23:41:33 -0400

    Added FD2 sub-tab

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.dynamic.spec.js
@@ -0,0 +1,22 @@
+describe("Test the harvest report dynamic values", () => {
+  beforeEach(() => {
+      cy.login("manager1", "farmdata2")
+      cy.visit("/farm/fd2-school/e2e")
+  })
+    
+  it("Check the generate report button", () => {
+    cy.get("[data-cy=report-title]").should("not.exist")
+    cy.get("[data-cy=generate-report-button]").click()
+    cy.get("[data-cy=report-title]").should("be.visible")
+
+  })
+
+  it("Check the generate report button values", () => {
+    cy.get("[data-cy=report-farm]").should("not.exist")
+    cy.get("[data-cy=generate-report-button]").click()
+    cy.get("[data-cy=report-farm]").should("have.text", "Farm:Sample Farm")
+
+    cy.get("[data-cy=report-user]").should("contain.text", "User:")
+    cy.get("[data-cy=report-language]").should("have.text", "English")
+  })
+})
