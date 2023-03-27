e5ba71522cf6c806632b5b59bf38df6474db5c85
JinLeeGG <leejo@dickinson.edu>
2023-03-27 01:32:20 -0400

    added cypress tests that checks headers of harvest report

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -4,7 +4,13 @@ describe("Test contents of harvest report table", () => {
       cy.visit("/farm/fd2-school/FD2")
   })
 
-  it("", () => {
-
+  it("Test that all column headers are correct", () => {
+    cy.get("[data-cy=generate-report-button]").click()
+    cy.get("[data-cy=h0]").should("have.text","ID")
+    cy.get("[data-cy=h1]").should("have.text","Date")
+    cy.get("[data-cy=h2]").should("have.text","Area")
+    cy.get("[data-cy=h3]").should("have.text","Crop")
+    cy.get("[data-cy=h4]").should("have.text","Yield")
+    cy.get("[data-cy=h5]").should("have.text","Unit")
   })
 })
