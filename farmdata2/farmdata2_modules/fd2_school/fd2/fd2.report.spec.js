37f42a43f3b7ce5b43e553ddc76ce79118db1381
EliasBerhe <eliastes123@gmail.com>
2023-03-28 14:55:06 -0400

    created FD2 sub-tab

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.report.spec.js
@@ -0,0 +1,23 @@
+describe("Test the harvest report Generations", () => {
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-school/e2e")
+       
+    })
+      
+    it("Check the harvest report generated", () => {
+        cy.get("[data-cy=report-title]").should('not.exist')
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=report-title]").should('be.visible')
+       
+    })
+
+    it("Check the harvest report generated details", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=farm]").should('have.text', 'Farm: Sample Farm')
+        cy.get("[data-cy=username]").should('contain.text', 'manager1')
+        cy.get("[data-cy=language]").should('have.text', 'English')
+       
+    })
+    
+})
