0269645d038308e0ff7c5d976bbcf097213981ff
Alexandrialexie <lexielb28@gmail.com>
2023-03-27 22:50:14 -0400

    Tests now pass

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.defaults.spec.js
@@ -1,12 +1,12 @@
 describe("Test the harvest report default values", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
-        cy.visit("/farm/fd2-school/e2e")
+        cy.visit("/farm/fd2-school/fd2")
     })
       
     it("Check the page header", () => {
         cy.get("[data-cy=page-header]")
-            .should("have.text","Harvest Report")
+            .should("have.text","My Harvest Report")
 
     })
 
@@ -16,9 +16,9 @@ describe("Test the harvest report default values", () => {
     })
 
     it("Check the crop drop-down", () => {
-        cy.get("[data-cy=crop-drop-down]").children().eq(0).should("have.text", "ARUGULA")
-        cy.get("[data-cy=crop-drop-down]").children().eq(4).should("have.text", "BEAN-FAVA")
-        cy.get("[data-cy=crop-drop-down]").children().eq(110).should("have.text", "ZUCCHINI")
-        cy.get("[data-cy=crop-drop-down]").children().should("have.length", "111")
+        cy.get("[data-cy=crop-drop-down] > [data-cy=dropdown-input] > [data-cy=option0]").should("have.text", "All")
+        cy.get("[data-cy=crop-drop-down] > [data-cy=dropdown-input] > [data-cy=option5]").should("have.text", "BEAN-FAVA")
+        cy.get("[data-cy=crop-drop-down] > [data-cy=dropdown-input] > [data-cy=option111]").should("have.text", "ZUCCHINI")
+        cy.get("[data-cy=crop-drop-down] > [data-cy=dropdown-input]").children().should("have.length", "112")
     })
 })
