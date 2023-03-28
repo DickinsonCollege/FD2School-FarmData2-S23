34cdbbf1f6abf59024c483ccacad506c11d44b9b
Alexandrialexie <lexielb28@gmail.com>
2023-03-27 23:06:31 -0400

    Added a test file for the table

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -0,0 +1,13 @@
+describe("Test the harvest report dynamic values", () => {
+  beforeEach(() => {
+      cy.login("manager1", "farmdata2")
+      cy.visit("/farm/fd2-school/fd2")
+  })
+    
+  it("Check the table", () => {
+    
+
+  })
+
+
+})
