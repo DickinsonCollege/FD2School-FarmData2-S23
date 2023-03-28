77a54558137d9830c46dd3f8f6ee81d602aa0edf
udvale <enkhtaiu@dickinson.edu>
2023-03-28 19:16:12 -0400

    Cypress testing whether the table headers have the correct names

--- farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
@@ -1,9 +1,17 @@
 describe("Check if the custom-table is displaying right", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
-        cy.visit("/farm/fd2-school/fd2")
+        cy.visit("/farm/fd2-school/FD2")
+        cy.get("[data-cy = generate-report-button]").click()
     })
-    it("Details should be not visible", () => {
-        cy.get("[data-cy = reportTable]").should("not.exist")
+    it("Check whether the table header is correct", () => {
+        cy.get("[data-cy=h0]").should("have.text","ID")
+        cy.get("[data-cy=h1]").should("have.text","Date")
+        cy.get("[data-cy=h2]").should("have.text","Area")
+        cy.get("[data-cy=h3]").should("have.text","Crop")
+        cy.get("[data-cy=h4]").should("have.text","Yield")
+        cy.get("[data-cy=h5]").should("have.text","Units")
     })
+
+
 })
