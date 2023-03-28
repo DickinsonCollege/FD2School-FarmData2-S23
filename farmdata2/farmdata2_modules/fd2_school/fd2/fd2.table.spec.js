fe8fd58bc9e0d85a540e582bf44ec283a360e9b2
EliasBerhe <eliastes123@gmail.com>
2023-03-28 18:55:40 -0400

    tested the number of columns

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -12,6 +12,7 @@ describe("Test the harvest table", () => {
         cy.get("[data-cy=h3]").should("have.text","Crop")
         cy.get("[data-cy=h4]").should("have.text","Yield")
         cy.get("[data-cy=h5]").should("have.text","Units")
+        cy.get("[data-cy=table-headers]").children().should("have.length", 6)
 
 
     })
