00a68ab31699acda2af1fc2246781ca32c626a44
EliasBerhe <eliastes123@gmail.com>
2023-03-28 18:38:37 -0400

    created a new test file fd2.table.spec.js

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -0,0 +1,12 @@
+describe("Test the harvest table", () => {
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-school/FD2")
+    })
+      
+    it("", () => {
+       
+
+    })
+   
+})
