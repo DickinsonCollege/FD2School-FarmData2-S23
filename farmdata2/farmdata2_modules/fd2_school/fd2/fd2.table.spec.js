37ccb6ca8fbe10d02c1d7720c905ba94b4106272
EliasBerhe <eliastes123@gmail.com>
2023-03-28 18:50:30 -0400

    tested all the table headers

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -4,8 +4,15 @@ describe("Test the harvest table", () => {
         cy.visit("/farm/fd2-school/FD2")
     })
       
-    it("", () => {
-       
+    it("Check table columns", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=h0]").should("have.text","ID")
+        cy.get("[data-cy=h1]").should("have.text","Date")
+        cy.get("[data-cy=h2]").should("have.text","Area")
+        cy.get("[data-cy=h3]").should("have.text","Crop")
+        cy.get("[data-cy=h4]").should("have.text","Yield")
+        cy.get("[data-cy=h5]").should("have.text","Units")
+
 
     })
    
