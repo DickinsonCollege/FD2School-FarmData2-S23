6a402904d2636b7e499a7954a5f5a3a6ef1b634c
thorpIV <thorpg@dickinson.edu>
2023-03-28 21:03:00 -0400

    Added FD2 tab to fd2_school.module and FD2 School Tab

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.generateReport.spec.js
@@ -0,0 +1,31 @@
+describe("Test the Generate Report Button", () => { 
+  beforeEach(() => { 
+      cy.login("manager1", "farmdata2") 
+      cy.visit("/farm/fd2-school/e2e") 
+    
+  }) 
+
+  it("Check Generate Report before generated", () => {      
+    cy.get("[data-cy=report-title]").should("not.exist")
+  }) 
+
+  it("Check Generate Report after generated", () => {      
+      cy.get("[data-cy=generate-report-button]").click()
+      cy.get("[data-cy=report-title]").should("be.visible")
+  }) 
+
+  it("Check Farm name after generated", () => {      
+    cy.get("[data-cy=generate-report-button]").click()
+    cy.get("[data-cy=farm-name]").should("contain.text", "Sample FarmName")
+  })
+
+  it("Check Language after generated", () => {      
+    cy.get("[data-cy=generate-report-button]").click()
+    cy.get("[data-cy=language]").should("have.text", "Language:English")
+  })
+
+  it("Check UserID after generated", () => {      
+    cy.get("[data-cy=generate-report-button]").click()
+    cy.get("[data-cy=user-id]").should("have.text", "User:manager1")
+  })
+})
