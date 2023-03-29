6cef2d3c3cef91e27139a6ce778c5c359256acf8
mikek16 <krausem@dickinson.edu>
2023-03-29 19:48:49 -0400

    added blank cypress test

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -0,0 +1,12 @@
+describe("Test the custom-table Element", () => {
+
+    beforeEach(() =>{
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-school/fd2")
+    })
+    it("Checks generate report button", () =>{
+
+    })
+
+    
+})
