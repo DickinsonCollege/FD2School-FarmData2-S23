6239384cbc1893389af8b39127abaf8cbc05768a
pranavm2109 <mishrap@dickinson.edu>
2023-03-28 23:10:01 -0400

    updated the beforeEach block to visit fd2 sub-tab instead of e2e

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.interactions.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.interactions.spec.js
@@ -1,7 +1,7 @@
 describe("Test user interactions in the harvest report", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
-        cy.visit("/farm/fd2-school/e2e")
+        cy.visit("/farm/fd2-school/fd2")
     })
 
     it("Test button to generate report", () => {
