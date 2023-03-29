8e5c674885009a235b871e9083949d4717c01026
mikek16 <krausem@dickinson.edu>
2023-03-29 13:03:35 -0400

    Spike 2 dropdown cypress test

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.defaults.spec.js
@@ -12,10 +12,9 @@ describe("Test the harvest report default values", () =>{
         cy.get("[data-cy=end-date]").should('have.value', "2020-05-15")
     })
     it("Check the crop dropdown list", () => {
-        cy.get("[data-cy=crop-dropdown]").children().eq(0).should("have.value", "ARUGULA")
-        cy.get("[data-cy=crop-dropdown]").children().eq(4).should("have.value", "BEAN-FAVA")
-        cy.get("[data-cy=crop-dropdown]").children().eq(110).should("have.value", "ZUCCHINI")
-        cy.get("[data-cy=crop-dropdown]").children().should("have.length", "111")
+        cy.get("[data-cy=option1]").should('have.value', 'ARUGULA')
+        cy.get("[data-cy=option5]").should("have.value", "BEAN-FAVA")
+        cy.get("[data-cy=option111]").should("have.value", "ZUCCHINI")
     })
 }       
       
