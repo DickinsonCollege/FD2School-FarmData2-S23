1c7b5df324f4156366fc31b92ec57e2e2e72a251
mikek16 <krausem@dickinson.edu>
2023-03-29 12:49:34 -0400

    added cypress test files

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.defaults.spec.js
@@ -0,0 +1,23 @@
+describe("Test the harvest report default values", () =>{
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-school/fd2")
+
+    })
+    it("Check the page Header", () =>{
+        cy.get("[data-cy=page-header]").should('have.text', "Harvest Report")
+    })
+    it("Check start and end dates", () =>{
+        cy.get("[data-cy=start-date]").should('have.value', "2020-05-05")
+        cy.get("[data-cy=end-date]").should('have.value', "2020-05-15")
+    })
+    it("Check the crop dropdown list", () => {
+        cy.get("[data-cy=crop-dropdown]").children().eq(0).should("have.value", "ARUGULA")
+        cy.get("[data-cy=crop-dropdown]").children().eq(4).should("have.value", "BEAN-FAVA")
+        cy.get("[data-cy=crop-dropdown]").children().eq(110).should("have.value", "ZUCCHINI")
+        cy.get("[data-cy=crop-dropdown]").children().should("have.length", "111")
+    })
+}       
+      
+
+)
---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.harvest.report.spec.js
@@ -0,0 +1,19 @@
+describe("Test the Harvest Report Generation", () => {
+
+    beforeEach(() =>{
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-school/fd2")
+    })
+    it("Checks generate report button", () =>{
+        cy.get("[data-cy=report-header]").should("not.exist")
+        cy.get("[data-cy=generate-report]").click()
+        cy.get("[data-cy=report-header]").should("be.visible")
+    })
+    it("Checks the Harvest Report Details", () => {
+        cy.get("[data-cy=generate-report]").click()
+        cy.get("[data-cy=farm-li]").should("have.text", "Farm: Sample Farm")
+        cy.get("[data-cy=user-li]").should("contain.text", "manager1")
+        cy.get("[data-cy=language-li]").should("have.text","English")
+    })
+    
+})
