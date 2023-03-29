a095d689bd3cfa6e96a18329c7b429fd39014221
aliouas <aliouas@dickinson.edu>
2023-03-29 06:42:56 -0400

    tested the farm name + username + language

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.generateReport.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.generateReport.spec.js
@@ -4,12 +4,22 @@ describe("Test the harvest report generate report button", () => {
         cy.visit("/farm/fd2-school/e2e") 
     }) 
 
-    it("Check the page header", () => { 
+    it("Check the report header", () => { 
         cy.get("[data-cy=report-header]")
         .should("not.exist")
         cy.get("[data-cy=generate-report]").click()
         cy.get("[data-cy=report-header]")
         .should("be.visible")
+    }) 
+
+    it("Check the farm name, username, and language", () => { 
+        cy.get("[data-cy=generate-report]").click()
+        cy.get("[data-cy=farm-name]")
+        .should("have.text","Farm:Sample Farm")
+        cy.get("[data-cy=report-username]")
+        .should("contain.text","manager1")
+        cy.get("[data-cy=report-language]")
+        .should("have.text","English")
     }) 
 
 }) 
