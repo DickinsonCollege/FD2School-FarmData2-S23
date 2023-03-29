c2ef3ac94c5d6b8d84a9d1640ce8ceadf679694a
thorpIV <thorpg@dickinson.edu>
2023-03-29 08:21:56 -0400

    Added the column test to the report table of the test on the FD2 tab

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -13,4 +13,10 @@ describe("Test report table", () => {
     cy.get("[data-cy=h4]").should("have.text","Yield")
     cy.get("[data-cy=h5]").should("have.text","Unit")
 })
+
+it("Test number of columns", () => {
+  cy.get("[data-cy=generate-report-button]").click()
+  cy.get("[data-cy=table-headers]").children().should("have.length", 6)
+})
+
 })
