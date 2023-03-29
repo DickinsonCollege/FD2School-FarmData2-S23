d4bd23fbeee479847ef33748a4ff4806aaec4f71
pranavm2109 <mishrap@dickinson.edu>
2023-03-28 23:40:49 -0400

    tested DropdownWithAllComponent in fd2 subtab

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.defaults.spec.js
@@ -18,17 +18,20 @@ describe("Test the harvest report default values", () => {
     })
 
     it("Check the contents of the crop dropdown menu", () => {
-        cy.get("[data-cy=crop-dropdown-menu]").children().eq(0)
+        cy.get("[data-cy=crop-dropdown-menu] > [data-cy=dropdown-input] > [data-cy=option0]")
+            .should("have.value", "All")
+
+        cy.get("[data-cy=crop-dropdown-menu] > [data-cy=dropdown-input] > [data-cy=option1]")
             .should("have.value", "ARUGULA")
 
-        cy.get("[data-cy=crop-dropdown-menu]").children().eq(4)
+        cy.get("[data-cy=crop-dropdown-menu] > [data-cy=dropdown-input] > [data-cy=option5]")
             .should("have.value", "BEAN-FAVA")
 
-        cy.get("[data-cy=crop-dropdown-menu]").children().eq(110)
+        cy.get("[data-cy=crop-dropdown-menu] > [data-cy=dropdown-input] > [data-cy=option111]")
             .should("have.value", "ZUCCHINI")
 
-        cy.get("[data-cy=crop-dropdown-menu]").children()
-            .should("have.length", "111")
+        cy.get(("[data-cy=crop-dropdown-menu] > [data-cy=dropdown-input]")).children()
+            .should("have.length", "112")
 
     })
 })
