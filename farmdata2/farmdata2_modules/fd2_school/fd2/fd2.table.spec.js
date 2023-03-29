83b302dc73532f2c79139f215bb3aade88c42f2c
Alexandrialexie <lexielb28@gmail.com>
2023-03-28 21:57:56 -0400

    Added header tests

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -2,11 +2,17 @@ describe("Test the harvest report dynamic values", () => {
   beforeEach(() => {
       cy.login("manager1", "farmdata2")
       cy.visit("/farm/fd2-school/fd2")
+      cy.get("[data-cy=generate-report-button]").click()
   })
     
   it("Check the table", () => {
     
-
+    //cy.get("[data-cy=h2]").should("have.text","Row")
+    cy.get("[data-cy=h0]").should("have.text","Date")
+    cy.get("[data-cy=h1]").should("have.text","Area")
+    cy.get("[data-cy=h2]").should("have.text","Crop")
+    cy.get("[data-cy=h3]").should("have.text","Yield")
+    cy.get("[data-cy=h4]").should("have.text","Units")
   })
 
 
