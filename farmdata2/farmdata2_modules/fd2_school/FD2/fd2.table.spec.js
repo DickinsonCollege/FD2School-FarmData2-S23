6266c752c240fb2f256ffc3ccbec40d5624fbeaa
udvale <enkhtaiu@dickinson.edu>
2023-03-28 21:52:15 -0400

    Tested table row length and correct crop value

--- farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
@@ -2,9 +2,9 @@ describe("Check if the custom-table is displaying right", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
         cy.visit("/farm/fd2-school/FD2")
-        cy.get("[data-cy = generate-report-button]").click()
     })
     it("Check whether the table header is correct", () => {
+        cy.get("[data-cy = generate-report-button]").click()
         cy.get("[data-cy=h0]").should("have.text","ID")
         cy.get("[data-cy=h1]").should("have.text","Date")
         cy.get("[data-cy=h2]").should("have.text","Area")
@@ -14,9 +14,11 @@ describe("Check if the custom-table is displaying right", () => {
         cy.get("[data-cy=table-headers]").children().should("have.length", 6)
     })
     it("Check whether the table is showing correct contents", () => {
-        
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input]").select("COLLARDS")
+        cy.get("[data-cy=area-dropdown]").select("GHANA-2")
+        cy.get("[data-cy = generate-report-button]").click()
+        cy.get("[data-cy=table]").children().should("have.length", 2)
+        cy.get("[data-cy=td-r0c3]").should("have.text", 'COLLARDS     ')
+        cy.get("[data-cy=td-r1c3]").should("have.text", 'COLLARDS     ')
     })
-
-
-
 })
