68d7386cc69a07a8bf5de26ae8a635c6ffeb2315
Foogabob <locker@dickinson.edu>
2023-03-29 12:31:33 -0400

    added cypress tests for and name and default start and end values on the harvest report

---
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -0,0 +1,19 @@
+describe("Test the harvest report default values", () => {
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-school/e2e")
+    })
+      
+    it("Check the page header", () => {
+        cy.get("[data-cy=page-header]")
+            .should("have.text", "Harvest Report")
+    })
+
+    it("Check if start and end dates are correct", () => {
+        cy.get("[data-cy=Start-date]")
+            .should("have.value", "2020-05-05")
+        cy.get("[data-cy=End-date]")
+            .should("have.value", "2020-05-15")
+
+    })
+})
