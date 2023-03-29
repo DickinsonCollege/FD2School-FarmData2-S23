d62bed623dc25d06292c91c871a89cee788aa562
Alexandrialexie <lexielb28@gmail.com>
2023-03-28 22:10:40 -0400

    Added number of rows test

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -13,6 +13,8 @@ describe("Test the harvest report dynamic values", () => {
     cy.get("[data-cy=h2]").should("have.text","Crop")
     cy.get("[data-cy=h3]").should("have.text","Yield")
     cy.get("[data-cy=h4]").should("have.text","Units")
+
+    cy.get("[data-cy=table-headers]").children().should("have.length", "6")
   })
 
 
