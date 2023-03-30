053f64e1fa736eb04c59b9f3ab2b3a9380502ce1
mikek16 <krausem@dickinson.edu>
2023-03-30 17:28:05 -0400

    Initial commit, adding seeding input data cypress test

---
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.data.defaults.spec.js
@@ -0,0 +1,16 @@
+const dayjs = require('dayjs')
+
+describe("Test Data section of Seeding Input form", () =>{
+    beforeEach(() => {
+        cy.login('manager1', 'farmdata2')
+        
+        // Cypress clears the local storage between each test.  
+        // So we need to save it at the end of each test (see afterEach)
+        // and then restore beore each test (here). 
+        cy.restoreLocalStorage() 
+        cy.visit('/farm/fd2-field-kit/seedingInput')
+    }) 
+    it("Checks the Data header", () => {
+        
+    })
+})
