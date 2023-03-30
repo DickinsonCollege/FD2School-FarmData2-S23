df26eef4970de16c8114e8ed6ff4cb5752fce4cc
aliouas <aliouas@dickinson.edu>
2023-03-30 04:30:03 -0400

    tests update beforeEach + corrected crop dropdown

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.defaults.spec.js
@@ -1,7 +1,7 @@
 describe("Test the harvest report default values", () => { 
     beforeEach(() => { 
         cy.login("manager1", "farmdata2") 
-        cy.visit("/farm/fd2-school/e2e") 
+        cy.visit("/farm/fd2-school/fd2") 
     }) 
 
     it("Check the page header", () => { 
@@ -17,14 +17,14 @@ describe("Test the harvest report default values", () => {
     }) 
 
     it("Check the crop dropdown", () => { 
-        cy.get("[data-cy=crop-dropdown]").children().eq(0)
-        .should("have.text","ARUGULA")
-        cy.get("[data-cy=crop-dropdown]").children().eq(4)
-        .should("have.text","BEAN-FAVA")
-        cy.get("[data-cy=crop-dropdown]").children().eq(110)
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option0]") 
+        .should("have.text","All")
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option4]")
+        .should("have.text","BEAN-DRY")
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option111]")
         .should("have.text","ZUCCHINI")
-        cy.get("[data-cy=crop-dropdown]").children()
-        .should("have.length",111)
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input]").children()
+        .should("have.length",112)
     }) 
 
 }) 
