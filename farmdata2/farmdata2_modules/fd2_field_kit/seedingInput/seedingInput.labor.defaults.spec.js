e3dad81e0163f1c6fd74a78214f21625fa6d43eb
sidlamsal <lamsals@dickinson.edu>
2023-03-29 22:59:10 -0400

    added spec.js file

---
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.labor.defaults.spec.js
@@ -0,0 +1,11 @@
+describe('Test the labor input section', () => {
+
+    beforeEach(() => {
+        cy.login('manager1', 'farmdata2')
+        cy.visit('/farm/fd2-field-kit/seedingInput')
+    }) 
+
+    it("Empty", () => {
+
+    })
+})
