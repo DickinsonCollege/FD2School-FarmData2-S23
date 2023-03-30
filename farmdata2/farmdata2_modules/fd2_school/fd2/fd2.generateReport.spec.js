bf10002a467ddd45f16914123bc7581e70be4af6
aliouas <aliouas@dickinson.edu>
2023-03-30 03:32:32 -0400

    created the FD2 sub-tab

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.generateReport.spec.js
@@ -0,0 +1,25 @@
+describe("Test the harvest report generate report button", () => { 
+    beforeEach(() => { 
+        cy.login("manager1", "farmdata2") 
+        cy.visit("/farm/fd2-school/e2e") 
+    }) 
+
+    it("Check the report header", () => { 
+        cy.get("[data-cy=report-header]")
+        .should("not.exist")
+        cy.get("[data-cy=generate-report]").click()
+        cy.get("[data-cy=report-header]")
+        .should("be.visible")
+    }) 
+
+    it("Check the farm name, username, and language", () => { 
+        cy.get("[data-cy=generate-report]").click()
+        cy.get("[data-cy=farm-name]")
+        .should("have.text","Farm:Sample Farm")
+        cy.get("[data-cy=report-username]")
+        .should("contain.text","manager1")
+        cy.get("[data-cy=report-language]")
+        .should("have.text","English")
+    }) 
+
+}) 
