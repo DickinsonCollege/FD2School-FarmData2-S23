483587f563fd007b0c97cf43191ab860d37f245f
mikek16 <krausem@dickinson.edu>
2023-03-29 20:10:44 -0400

    added table header check cypress test

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -4,9 +4,15 @@ describe("Test the custom-table Element", () => {
         cy.login("manager1", "farmdata2")
         cy.visit("/farm/fd2-school/fd2")
     })
-    it("Checks generate report button", () =>{
-
+    it("Checks Table Headers", () =>{
+        cy.get("[data-cy=generate-report]").click()
+        cy.get("[data-cy=h0]").should("have.text","Row")
+        cy.get("[data-cy=h1]").should("have.text","Date")
+        cy.get("[data-cy=h2]").should("have.text","Area")
+        cy.get("[data-cy=h3]").should("have.text","Crop")
+        cy.get("[data-cy=h4]").should("have.text","Yield")
+        cy.get("[data-cy=h5]").should("have.text","Units")
+        cy.get("[data-cy=table-headers]").children().should('have.length',7)
     })
-
-    
+   
 })
