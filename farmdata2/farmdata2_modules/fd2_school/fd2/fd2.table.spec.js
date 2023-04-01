0e6c438c058bfee30e2e18ba171884a35443ff69
jamesng5 <nguyenja@dickinson.edu>
2023-04-01 02:59:11 -0400

    add cypress test file for table in the report

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -0,0 +1,10 @@
+describe("Test the harvest table report", () =>{
+    beforeEach(()=>{
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-school/fd2/fd2.html")
+    })
+
+    it("", () =>{
+        
+    })
+})
