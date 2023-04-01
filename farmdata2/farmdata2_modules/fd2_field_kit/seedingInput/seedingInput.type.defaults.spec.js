2472fd722b1f493c0b62d114f4949058b576640a
pranavm2109 <mishrap@dickinson.edu>
2023-04-01 18:01:17 -0400

    created file to test seeding input type defaults

---
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.type.defaults.spec.js
@@ -0,0 +1,9 @@
+describe("Test the harvest report default values", () => {
+    beforeEach(() => {
+        
+    })
+      
+    it("Tests whether Tray element is enabled", () => {
+    
+    })
+})
