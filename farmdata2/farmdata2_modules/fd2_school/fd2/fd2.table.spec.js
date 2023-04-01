deeca24a6d3fd9fc7debd78d29b6b9d698e6d18d
jamesng5 <nguyenja@dickinson.edu>
2023-04-01 03:07:00 -0400

    cypress test to check the header of the report table

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -4,7 +4,13 @@ describe("Test the harvest table report", () =>{
         cy.visit("/farm/fd2-school/fd2/fd2.html")
     })
 
-    it("", () =>{
-        
+    it("Check table headers", () =>{
+        cy.get("[data-cy=generate-button]").click()
+        cy.get("[data-cy=h0]").should("have.text", "Row")
+        cy.get("[data-cy=h1]").should("have.text", "Date")
+        cy.get("[data-cy=h2]").should("have.text", "Area")
+        cy.get("[data-cy=h3]").should("have.text", "Crop")
+        cy.get("[data-cy=h4]").should("have.text", "Yield")
+        cy.get("[data-cy=h5]").should("have.text", "Units")
     })
 })
