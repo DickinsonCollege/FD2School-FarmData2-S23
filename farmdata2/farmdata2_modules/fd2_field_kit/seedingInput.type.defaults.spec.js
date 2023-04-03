4ac113addd974f44820839d3d33bee8930f2e09e
andrewscheiner53 <scheinea@dickinson.edu>
2023-04-03 09:45:11 -0400

    Our team's test shows up in correct folder so we can run in Cypress

---
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput.type.defaults.spec.js
@@ -0,0 +1,12 @@
+describe("Test the harvest report default values", () => {
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-field-kit/seedingInput")
+    })
+      
+    it("Tests whether Tray element is enabled", () => {
+    
+    })
+
+    
+})
