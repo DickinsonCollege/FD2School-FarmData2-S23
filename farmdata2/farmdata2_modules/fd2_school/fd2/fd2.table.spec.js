564aa646425c6644cf13409b4e9053a320a783ef
won369369 <wj0293@gmail.com>
2023-04-02 23:42:24 -0400

    Created a new test file named fd2.table.spec.js

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -0,0 +1,9 @@
+describe("Test the harvest report table", () => {
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-school/fd2")
+    })
+      
+    it("Check the table headers", () => {
+    })
+})
