46411288018d33d9f79aea68153cbdf6f381c044
won369369 <wj0293@gmail.com>
2023-04-02 22:11:42 -0400

    Check <option> elements 0, 1, 5 and the last one and Check the number of <option> elements.

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.defaults.spec.js
@@ -1,7 +1,7 @@
 describe("Test the harvest report default values", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
-        cy.visit("/farm/fd2-school/e2e")
+        cy.visit("/farm/fd2-school/fd2")
     })
       
     it("Check the page header", () => {
@@ -17,11 +17,13 @@ describe("Test the harvest report default values", () => {
     })
 
     it("Check the crops in the crop drop down", () => {
-        let list = cy.get("[data-cy=crop-dropdown]").children()
-        
-        cy.get("[data-cy=crop-dropdown]").children().eq(0).should("have.text","ARUGULA")
-        cy.get("[data-cy=crop-dropdown]").children().eq(4).should("have.text","BEAN-FAVA")
-        cy.get("[data-cy=crop-dropdown]").children().eq(110).should("have.text","ZUCCHINI")
-        cy.get("[data-cy=crop-dropdown]").children().should("have.length", 111)
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option0]").should("have.text","All")
+
+        cy.get("[data-cy=option0]").should("have.text","All")
+
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option1]").should("have.text","ARUGULA")
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option4]").should("have.text","BEAN-DRY")
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input] > [data-cy=option111]").should("have.text","ZUCCHINI")
+        cy.get("[data-cy=crop-dropdown] > [data-cy=dropdown-input]").children().should("have.length", 112)
     })
 })
