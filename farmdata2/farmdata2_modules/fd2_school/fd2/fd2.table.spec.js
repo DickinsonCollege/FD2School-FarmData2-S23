832daaa6bfc7d497a7cea4df14c984d7df96718d
won369369 <wj0293@gmail.com>
2023-04-02 23:45:45 -0400

    a test that checks that all of the table headers are correct.

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -5,5 +5,11 @@ describe("Test the harvest report table", () => {
     })
       
     it("Check the table headers", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=h0]").should("have.text","Date")
+        cy.get("[data-cy=h1]").should("have.text","Area")
+        cy.get("[data-cy=h2]").should("have.text","Crop")
+        cy.get("[data-cy=h3]").should("have.text","Yield")
+        cy.get("[data-cy=h4]").should("have.text","Units")
     })
 })
