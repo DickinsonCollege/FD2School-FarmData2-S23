ec659343b4bdad8c22624c61cfee56736cf3a7d0
saiatl2354 <atluris@dickinson.edu>
2023-04-03 22:41:41 -0400

    headers and columns tests

--- farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
@@ -3,7 +3,16 @@ describe("Harvest Table Test", () => {
     cy.login("manager1", "farmdata2")
     cy.visit("/farm/fd2-school/FD2")
   })
-  it("", () => {
-    
+  it("Test all column headers", () => {
+    cy.get("[data-cy=generate-report-button]").click()
+    cy.get("[data-cy=h0]").should("have.text", "ID")
+    cy.get("[data-cy=h0]").should("have.text", "Row")
+    cy.get("[data-cy=h1]").should("have.text", "Date")
+    cy.get("[data-cy=h2]").should("have.text", "Area")
+    cy.get("[data-cy=h3]").should("have.text", "Crop")
+    cy.get("[data-cy=h4]").should("have.text", "Yield")
+    cy.get("[data-cy=h5]").should("have.text", "Unit")
+    cy.get("[data-cy=table-headers]").children().should("have.length",0)
   })
 })
+
