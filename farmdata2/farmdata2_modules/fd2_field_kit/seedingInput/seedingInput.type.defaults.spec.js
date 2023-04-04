c59ac57af8422cea696cd23bd995e406a3dea2bc
won369369 <wj0293@gmail.com>
2023-04-03 22:59:35 -0400

    Tests must check that the Direct Seeding section of the Seeding Input Form appears when "Direct" is selected

--- farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.type.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.type.defaults.spec.js
@@ -1,11 +1,20 @@
 describe("Test if the window appears if Direct is selected", () => {
-  
+
   beforeEach(() => {
+    cy.login('manager1', 'farmdata2')
 
+    // Cypress clears the local storage between each test.  
+    // So we need to save it at the end of each test (see afterEach)
+    // and then restore beore each test (here). 
+    cy.restoreLocalStorage()
+    cy.visit('/farm/fd2-field-kit/seedingInput')
   })
 
-  it("tests when Direct button is selected", () => {
+  it('should display the Direct Seeding section when Direct is selected', () => {
+    cy.get('[data-cy=direct-seedings]').check();
+    cy.get('[data-cy=num-rowbed-input]').should('be.visible');
+    cy.get('[data-cy=unit-feet]').should('be.visible');
+    cy.get('[data-cy=num-feet-input]').should('be.visible');
+  });
 
-
-  })
 })
