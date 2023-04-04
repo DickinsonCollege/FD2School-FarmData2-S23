11bb70c0d874eaa65837d7736b6d9ed9d85fff94
pranavm2109 <mishrap@dickinson.edu>
2023-04-03 21:53:50 -0400

    tested that neither the tray nor the direct element is selected

--- farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.type.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.type.defaults.spec.js
@@ -9,5 +9,10 @@ describe("Test the harvest report default values", () => {
         cy.get("[data-cy='direct-seedings']").should('be.enabled')
     })
 
+    it("Tests that neither the Tray nor the Direct element is selected", () => {
+        cy.get("[data-cy='tray-seedings']").should('not.be.selected')
+        cy.get("[data-cy='direct-seedings']").should('not.be.selected')
+    })
+
     
 })
