e33b4e0727c2104885bc55d35e9a4362167c7221
mikek16 <krausem@dickinson.edu>
2023-04-05 18:25:48 -0400

    combined similar 'it' blocks

--- farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.data.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.data.defaults.spec.js
@@ -11,25 +11,17 @@ describe("Test Data section of Seeding Input form", () =>{
     it("Checks the Data header", () => {
         cy.get("[data-cy=data-header").should('have.text',"Data")
     })
-
-    it("Checks the Date input element selected", () => {
+      
+    it("Checks the Date input defaults", () => {
         cy.get("[data-cy=date-selection").should('not.be.disabled')
-    })
-
-    it("Checks if selected date is current date", () => {
         cy.get('[data-cy=date-selection] input[type=date]').should('be.visible').should('have.value', dayjs().format('YYYY-MM-DD'));
     })
 
-    it("Checks if crop-dropdown is enabled", ()=>{
+    it("Checks the crop-dropdown defaults", ()=>{
         cy.get("[data-cy=crop-selection] > [data-cy=dropdown-input]").should("not.be.disabled")
+        
         cy.get("[data-cy=crop-selection] > [data-cy=dropdown-input]").should("have.text", "")
-    })
-
-    it("Checks if any crop-dropdown option has been selected ", ()=>{
-        cy.get("[data-cy=crop-selection] > [data-cy=dropdown-input]").should("have.text", "")
-    })    
-
-    it("Checks the crop dropdown is correct", () => {
+        
         cy.get("[data-cy=crop-selection] > [data-cy=dropdown-input] > [data-cy=option0").should("have.text","ARUGULA")
         cy.get("[data-cy=crop-selection] > [data-cy=dropdown-input] > [data-cy=option110").should("have.text","ZUCCHINI")
         cy.get("[data-cy=crop-selection] > select").find("option").should("have.length", 111);
