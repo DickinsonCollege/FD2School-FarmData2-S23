61ffc5ad3f95944e8b965c568eccdefeabbf7621
mikek16 <krausem@dickinson.edu>
2023-04-05 12:55:05 -0400

    Cleaning cypress test

--- farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.data.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.data.defaults.spec.js
@@ -5,44 +5,30 @@ describe("Test Data section of Seeding Input form", () =>{
 
     beforeEach(() => {
         cy.login('manager1', 'farmdata2')
-
-        
-        // Cypress clears the local storage between each test.  
-        // So we need to save it at the end of each test (see afterEach)
-        // and then restore beore each test (here). 
-        cy.restoreLocalStorage() 
         cy.visit('/farm/fd2-field-kit/seedingInput')
     }) 
 
-
-    //task #1
     it("Checks the Data header", () => {
         cy.get("[data-cy=data-header").should('have.text',"Data")
     })
 
-    //task #2
     it("Checks the Date input element selected", () => {
         cy.get("[data-cy=date-selection").should('not.be.disabled')
     })
 
-    //task #3
     it("Checks if selected date is current date", () => {
         cy.get('[data-cy=date-selection] input[type=date]').should('be.visible').should('have.value', dayjs().format('YYYY-MM-DD'));
     })
 
-
-    //task #6
     it("Checks the crop dropdown is correct", () => {
         cy.get("[data-cy=crop-selection] > [data-cy=dropdown-input")
             .children()
             .first().should("have.text", "ARUGULA")
-        
         cy.get("[data-cy=crop-selection] > [data-cy=dropdown-input")
             .children()
             .last().should("have.text", "ZUCCHINI")
         cy.get("[data-cy=crop-selection] > [data-cy=dropdown-input")
             .children()
             .should("have.length", "111")
-        
     })
 })
