6c930ad3a58c0f38c8151d8ac9b4606fd7a3fa0b
mikek16 <krausem@dickinson.edu>
2023-04-05 18:10:57 -0400

    Remove dropdown element reference via children function

--- farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.data.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.data.defaults.spec.js
@@ -20,27 +20,18 @@ describe("Test Data section of Seeding Input form", () =>{
         cy.get('[data-cy=date-selection] input[type=date]').should('be.visible').should('have.value', dayjs().format('YYYY-MM-DD'));
     })
 
-    // task #4
     it("Checks if crop-dropdown is enabled", ()=>{
         cy.get("[data-cy=crop-selection] > [data-cy=dropdown-input]").should("not.be.disabled")
         cy.get("[data-cy=crop-selection] > [data-cy=dropdown-input]").should("have.text", "")
     })
 
-    // task #5
     it("Checks if any crop-dropdown option has been selected ", ()=>{
         cy.get("[data-cy=crop-selection] > [data-cy=dropdown-input]").should("have.text", "")
     })    
 
-    //task #6
     it("Checks the crop dropdown is correct", () => {
-        cy.get("[data-cy=crop-selection] > [data-cy=dropdown-input")
-            .children()
-            .first().should("have.text", "ARUGULA")
-        cy.get("[data-cy=crop-selection] > [data-cy=dropdown-input")
-            .children()
-            .last().should("have.text", "ZUCCHINI")
-        cy.get("[data-cy=crop-selection] > [data-cy=dropdown-input")
-            .children()
-            .should("have.length", "111")
+        cy.get("[data-cy=crop-selection] > [data-cy=dropdown-input] > [data-cy=option0").should("have.text","ARUGULA")
+        cy.get("[data-cy=crop-selection] > [data-cy=dropdown-input] > [data-cy=option110").should("have.text","ZUCCHINI")
+        cy.get("[data-cy=crop-selection] > select").find("option").should("have.length", 111);
     })
 })
