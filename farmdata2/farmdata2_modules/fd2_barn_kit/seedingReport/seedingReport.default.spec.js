7e0e1ea41f16015960e9cfc8fe4ca33819214bc6
saiatl2354 <atluris@dickinson.edu>
2023-04-05 14:44:34 -0400

    generate button and set dates header test updates

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.default.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.default.spec.js
@@ -1,5 +1,17 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('eTest Seeding Report Default', () => {
+  beforeEach(()=> {
+    cy.login("manager1", "farmdata2")
+    cy.visit("/farm/fd2_barn_kit/seedingReport")
   })
+  
+  // it("Test the default start date and end date", ()=>{
+  //   cy.get("[data-cy=date-range-selection]").click()
+  //   cy.get("[data-cy=date-range-selection]").should('have.value','2023-01-01')
+  //   cy.get("[data-cy=date-range-selection]").should('have.value','2023-04-05')
+  // })
+
+  // it("Test the generate button", () => {
+  //   cy.get("[data-cy=generate-rpt-btn]").click()
+  //   cy.get("[data-cy=farm]").should("have.text", "Set Dates")
+  // })
 })
