ced09d597a05c2243424590337bc3aabf2c6f8ce
Foogabob <locker@dickinson.edu>
2023-04-05 14:31:33 -0400

    created a spec file for testing submit button behavior

---
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/TestSubmitButtonBehavior.spec.js
@@ -0,0 +1,7 @@
+describe('Test the test submit button behavior', () => {
+
+    beforeEach(() => {
+        cy.login('manager1', 'farmdata2')
+        cy.visit('/farm/fd2-field-kit/seedingInput')
+    }) 
+})
