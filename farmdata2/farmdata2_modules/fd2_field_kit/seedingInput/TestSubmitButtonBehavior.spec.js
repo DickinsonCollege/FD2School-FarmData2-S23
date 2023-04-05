b7002af292de094ae7f6c869baef2bfe43cca3cb
sidlamsal <lamsals@dickinson.edu>
2023-04-05 19:05:48 -0400

    Test submit button is initially disabled

--- farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/TestSubmitButtonBehavior.spec.js
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/TestSubmitButtonBehavior.spec.js
@@ -1,7 +1,14 @@
-describe('Test the test submit button behavior', () => {
+describe('Test the submit button behavior', () => {
 
     beforeEach(() => {
         cy.login('manager1', 'farmdata2')
         cy.visit('/farm/fd2-field-kit/seedingInput')
+        cy.waitForPage()
     }) 
+
+    it('Test submit button initially disabled', () => {
+        cy.get("[data-cy='submit-button']")
+            .should("be.disabled")
+    })
+
 })
