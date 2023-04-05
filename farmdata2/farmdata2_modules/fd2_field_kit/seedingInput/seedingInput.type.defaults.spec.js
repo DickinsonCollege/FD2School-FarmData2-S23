7e8741aa634b947b6f05f77bba3a023f0a3e8f2f
JinLeeGG <leejo@dickinson.edu>
2023-04-05 17:23:23 -0400

    test for dropdown area

--- farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.type.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.type.defaults.spec.js
@@ -1,5 +1,12 @@
 describe("Tests for seeding input", () => {
 
+  before(() => {
+    // Delete the crops and areas from local storge if it is there
+    // before running our tests.  
+    localStorage.removeItem('crops')
+    localStorage.removeItem('areas')
+  })
+
   beforeEach(() => {
     cy.login('manager1', 'farmdata2')
 
@@ -10,6 +17,30 @@ describe("Tests for seeding input", () => {
     cy.visit('/farm/fd2-field-kit/seedingInput')
   })
 
+  afterEach(() => {
+    // Save the local storage at the end of each test so 
+    // that it can be restored at the start of the next 
+    cy.saveLocalStorage()
+  })
+
+  it('test the first visit to the page (i.e. no cached crops and areas)', () => {
+    // This needs to be the first test run to work properly.
+    // First time through the crops and areas should not be cached.
+    // Note the cached value is cleared in the before().
+    let crops = localStorage.getItem('crops')
+    let areas = localStorage.getItem('areas')
+
+    expect(crops).to.equal(null)
+    expect(areas).to.equal(null)
+
+    // Set up intercepts to wait for the map to be loaded for caching. 
+    cy.intercept('GET', 'taxonomy_term?bundle=farm_crops&page=1').as('cropmap')
+    cy.intercept('GET', 'taxonomy_term.json?bundle=farm_areas').as('areamap') 
+    // Wait here for the maps to load in the page. 
+    cy.wait(['@cropmap', '@areamap']) 
+  })
+
+  //issue 159 sub-task 1
   it('should display the Direct Seeding section when Direct is selected', () => {
     cy.get('[data-cy=direct-seedings]').check();
     cy.get('[data-cy=num-rowbed-input]').should('be.visible');
@@ -36,5 +67,29 @@ describe("Tests for seeding input", () => {
       })
   })
 
-  
+
+  //issue 159 sub-task 2
+  it('test if areas are correctly loaded to the dropdown for direct seeding', () => {
+    // Applying the filter to the area dropdown for direct seeding
+    let areaArray = []
+    let areaResponse = JSON.parse(localStorage.getItem('areas'))
+    let directAreaOnly = areaResponse.filter((x) => x.area_type === 'field' || x.area_type === 'bed')
+    areaArray = directAreaOnly.map((h) => h.name)
+
+    cy.get('[data-cy=direct-seedings]')
+      .click()
+      .then(() => {
+        cy.get('[data-cy=direct-area-selection] > [data-cy=dropdown-input]')
+          .children() 
+          .first()
+          .should('have.value', 'A')
+        cy.get('[data-cy=direct-area-selection] > [data-cy=dropdown-input]')
+          .children() 
+          .last()  
+          .should('have.value', 'Z')
+        cy.get('[data-cy=direct-area-selection] > [data-cy=dropdown-input]')
+          .children() 
+          .should('have.length', areaArray.length)
+        })
+  })            
 })
