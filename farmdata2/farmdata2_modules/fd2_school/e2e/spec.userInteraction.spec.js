86ff56fcc494e902ed882d40524a788bdd46efa8
Foogabob <locker@dickinson.edu>
2023-04-04 22:46:48 -0400

    added user input test cases for the generate report button

--- farmdata2/farmdata2_modules/fd2_school/e2e/spec.userInteraction.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/spec.userInteraction.spec.js
@@ -11,4 +11,14 @@ describe("Test the harvest report User Inputs", () => {
     cy.get("[data-cy=report-header]")
       .should("be.visible")
   })
+
+  it("Check farm and user information", () =>{ 
+    cy.get("[data-cy=generate-report-button]").click()
+    cy.get("[data-cy=farm-name]")
+      .should("have.text", "Farm:Sample Farm")
+    cy.get("[data-cy=user-name]")
+      .should("contains.text", "manager1")
+      cy.get("[data-cy=language]")
+      .should("have.text", "English")
+  })
 })
