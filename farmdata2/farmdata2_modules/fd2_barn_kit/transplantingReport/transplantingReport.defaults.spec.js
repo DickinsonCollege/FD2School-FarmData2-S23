a42e1258d96907328db9036bc03b0b77d6adeaa9
udvale <enkhtaiu@dickinson.edu>
2023-04-08 18:15:50 -0400

    Tested page header and section label

--- farmdata2/farmdata2_modules/fd2_barn_kit/transplantingReport/transplantingReport.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/transplantingReport/transplantingReport.defaults.spec.js
@@ -3,7 +3,9 @@ describe("Testing Transplanting Report in BarnKit", () => {
         cy.login('manager1', 'farmdata2')
         cy.visit('/farm/fd2-barn-kit/transplantingReport')
     })
-    it("Check page before table is generated" ,() => {
+    it("Check page headers" ,() => {
+        cy.get("[data-cy = report-header]").should("have.text", "Transplanting Report")
+        cy.get("[data-cy = section-header]").should("have.text", "Set Dates")
     })
 
 })
