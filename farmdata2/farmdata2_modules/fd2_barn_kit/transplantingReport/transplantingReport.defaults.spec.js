e9fd05aadf6a7d0aa7a3191ac7552ac3dcc4b048
udvale <enkhtaiu@dickinson.edu>
2023-04-08 17:50:19 -0400

    Added spec.js file for testing transplating report

---
+++ farmdata2/farmdata2_modules/fd2_barn_kit/transplantingReport/transplantingReport.defaults.spec.js
@@ -0,0 +1,9 @@
+describe("Testing Transplanting Report in BarnKit", () => {
+    beforeEach(() => {
+        cy.login('manager1', 'farmdata2')
+        cy.visit('/farm/fd2-barn-kit/transplantingReport')
+    })
+    it("Check page before table is generated" ,() => {
+    })
+
+})
