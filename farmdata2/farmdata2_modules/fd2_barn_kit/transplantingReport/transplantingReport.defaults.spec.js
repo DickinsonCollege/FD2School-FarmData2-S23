7f4ff980e3dccabeb883b984fa60d93c3fa58c25
udvale <enkhtaiu@dickinson.edu>
2023-04-08 19:21:37 -0400

    Tested default start, end dates using dayjs

--- farmdata2/farmdata2_modules/fd2_barn_kit/transplantingReport/transplantingReport.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/transplantingReport/transplantingReport.defaults.spec.js
@@ -1,19 +1,32 @@
 const dayjs = require('dayjs')
+
 describe("Testing Transplanting Report in BarnKit", () => {
     beforeEach(() => {
         cy.login('manager1', 'farmdata2')
         cy.visit('/farm/fd2-barn-kit/transplantingReport')
     })
+
     it("Check page header and section label" ,() => {
         cy.get("[data-cy = report-header]").should("have.text", "Transplanting Report")
         cy.get("[data-cy = section-header]").should("have.text", "Set Dates")
     })
+
     it("The report table shouldn't be visible", () => {
         cy.get("[data-cy = report-table]").should("not.exist")
     })
+
     it("Check generate button in table", () => {
         cy.get("[data-cy = generate-rpt-btn]").should("have.text", "Generate Report")
         cy.get("[data-cy = generate-rpt-btn]").should("be.enabled")
     })
 
+    it("Check start and end date with current year", () => {
+        cy.get("[data-cy = end-date-select] > [data-cy = date-select]").click()
+        cy.get("[data-cy = start-date-select] > [data-cy = date-select]").click()
+
+        cy.get("[data-cy = date-range-selection] > [data-cy = end-date-select] > [data-cy = date-select]")
+        .should('have.value',dayjs().format("YYYY-MM-DD"))
+        cy.get("[data-cy = date-range-selection] > [data-cy = start-date-select] > [data-cy = date-select]")
+        .should('have.value',dayjs().format("YYYY-01-01"))
+    })
 })
