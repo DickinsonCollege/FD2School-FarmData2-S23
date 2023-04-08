d9c1796701c84acbb4b891ce1dd1b81b3c8a442e
Alexandrialexie <brownale@dickinson.edu>
2023-04-07 20:50:34 -0400

    Added an it block to test that the crop dropdown holds the correct crops for the selected date range

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.crop.filter.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.crop.filter.spec.js
@@ -6,7 +6,27 @@ describe("Test that the crop filter in the Seeding Report works as intended", ()
     })
 
     it("Tests that the dropdown for the crop filter only contains the crops that exist in the given date range", () => {
-        
+        cy.get('[data-cy=start-date-select]').type('2020-03-01')
+        cy.get('[data-cy=end-date-select]').type('2020-03-07')
+        cy.get('[data-cy=generate-rpt-btn]').click()
+
+        cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]')
+                .children()
+                .should("have.length", 5)
+
+        cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]')
+                .children()
+                .first()
+                .should('have.value', 'All')
+
+        cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]')
+                .children().next()
+                .should('have.value', 'BOKCHOY')
+
+        cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]')
+                .children()
+                .last()
+                .should('have.value', 'RADISH')
     })
 
 })
