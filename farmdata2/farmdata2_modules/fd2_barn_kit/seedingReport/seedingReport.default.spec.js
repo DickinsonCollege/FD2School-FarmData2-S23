c2f5756f3660fdac36f79bd81ba4361b13e9b257
saiatl2354 <atluris@dickinson.edu>
2023-04-12 10:24:22 -0400

    Test Page Header

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.default.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.default.spec.js
@@ -9,9 +9,13 @@ describe('eTest Seeding Report Default', () => {
   //   cy.get("[data-cy=date-range-selection]").should('have.value','2023-01-01')
   //   cy.get("[data-cy=date-range-selection]").should('have.value','2023-04-05')
   // })
+  it("Check that the page contains header", () => {
+    cy.get("[data-cy=generate-page-header]").click().should("have text", "Seeding Report")
+  })
 
   it("Test the generate button", () => {
     cy.get("[data-cy=generate-rpt-btn]").click()
+    cy.get('[data-cy=generate-rpt-btn]').should('exist')
   })
   it("Test to make sure that there are dates", () => {
     cy.get("[data-cy=farm]").should("have.text", "Set Dates")
