fcde591af16c0f53aea2b2fdeeb53b3f1eb45188
pranavm2109 <mishrap@dickinson.edu>
2023-04-12 12:19:01 -0400

    added it to test that report table contains only the logs pertaining to the selected crop

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.crop.filter.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.crop.filter.spec.js
@@ -19,22 +19,22 @@ describe("Test that the crop filter in the Seeding Report works as intended", ()
         cy.get('[data-cy=generate-rpt-btn]').click()
 
         cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]')
-                .children()
-                .should("have.length", 5)//There should be five options: the four crops in this date range and the "All" option.
+            .children()
+            .should("have.length", 5)//There should be five options: the four crops in this date range and the "All" option.
 
         cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]')
-                .children()
-                .first()
-                .should('have.value', 'All')
+            .children()
+            .first()
+            .should('have.value', 'All')
 
         cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]')
-                .children().next()
-                .should('have.value', 'BOKCHOY')
+            .children().next()
+            .should('have.value', 'BOKCHOY')
 
         cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]')
-                .children()
-                .last()
-                .should('have.value', 'RADISH')
+            .children()
+            .last()
+            .should('have.value', 'RADISH')
     })
 
     it("Tests that when 'All' crops are selected, the table will have seeding logs for several crops", () => {
@@ -45,11 +45,46 @@ describe("Test that the crop filter in the Seeding Report works as intended", ()
 
         //Check to ensure All is selected in the dropdown
         cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]').should('have.value', 'All')
- 
+
         //Check that several crops have logs in the table when All is selected
         cy.get('[data-cy=report-table]').contains('td', 'ENDIVE')
         cy.get('[data-cy=report-table]').contains('td', 'RADICCHIO')
         cy.get('[data-cy=report-table]').contains('td', 'BOKCHOY')
         cy.get('[data-cy=report-table]').contains('td', 'RADISH')
+    })
+
+    it("Tests that when a specific crop is selected, the table will have only the seeding logs for that crop", () => {
+        //Check date range 03/01/2020 - 09/30/2020
+        cy.get('[data-cy=start-date-select]').type('2020-03-01')
+        cy.get('[data-cy=end-date-select]').type('2020-09-30')
+        cy.get('[data-cy=generate-rpt-btn]').click()
+
+        //Selecting PEPPERS-HOT in the crops dropdown menu
+        cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]').select('PEPPERS-HOT')
+        //checking to ensure PEPPERS-HOT is selected in the dropdown menu
+        cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]').should('have.value', 'PEPPERS-HOT')
+        //Check that only logs pertaining to PEPPERS-HOT exist in the table
+        cy.get('[data-cy=report-table]').each((tableLog) => {
+            cy.wrap(tableLog).contains('td', "PEPPERS-HOT")
+        })
+
+        //Selecting WATERMELON in the crops dropdown menu
+        cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]').select('WATERMELON')
+        //checking to ensure WATERMELON is selected in the dropdown menu
+        cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]').should('have.value', 'WATERMELON')
+        //Check that only logs pertaining to WATERMELON exist in the table
+        cy.get('[data-cy=report-table]').each((tableLog) => {
+            cy.wrap(tableLog).contains('td', "WATERMELON")
+        })
+
+        //Selecting BEET in the crops dropdown menu
+        cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]').select('BEET')
+        //checking to ensure BEET is selected in the dropdown menu
+        cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]').should('have.value', 'BEET')
+        //Check that only logs pertaining to BEET exist in the table
+        cy.get('[data-cy=report-table]').each((tableLog) => {
+            cy.wrap(tableLog).contains('td', "BEET")
+        })
+
     })
 })
