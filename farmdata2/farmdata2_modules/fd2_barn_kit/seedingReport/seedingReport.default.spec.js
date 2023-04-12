a1676893416133268b7dfe30bca6137faceb1531
saiatl2354 <atluris@dickinson.edu>
2023-04-12 10:15:19 -0400

    Set dates header test

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.default.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.default.spec.js
@@ -1,17 +1,19 @@
 describe('eTest Seeding Report Default', () => {
-  beforeEach(()=> {
+  beforeEach(() => {
     cy.login("manager1", "farmdata2")
     cy.visit("/farm/fd2_barn_kit/seedingReport")
   })
-  
+
   // it("Test the default start date and end date", ()=>{
   //   cy.get("[data-cy=date-range-selection]").click()
   //   cy.get("[data-cy=date-range-selection]").should('have.value','2023-01-01')
   //   cy.get("[data-cy=date-range-selection]").should('have.value','2023-04-05')
   // })
 
-  // it("Test the generate button", () => {
-  //   cy.get("[data-cy=generate-rpt-btn]").click()
-  //   cy.get("[data-cy=farm]").should("have.text", "Set Dates")
-  // })
+  it("Test the generate button", () => {
+    cy.get("[data-cy=generate-rpt-btn]").click()
+  })
+  it("Test to make sure that there are dates", () => {
+    cy.get("[data-cy=farm]").should("have.text", "Set Dates")
+  })
 })
