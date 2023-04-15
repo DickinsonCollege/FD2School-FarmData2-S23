552949772bbf1fa05383664c3b39c2fd38bf060a
tainguyen103 <tainguyen103200@gmail.com>
2023-04-15 16:22:44 -0400

    spike 2 - tested default start and end dates

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -4,8 +4,15 @@ describe("Test the harvest report default values", () => {
         cy.visit("/farm/fd2-school/e2e")
     })
       
-    it("Check the page header", () => {
+    it("Check page header", () => {
         cy.get("[data-cy=page-header]")
         .should("have.text","Harvest Report")
+    })
+
+    it("Check default start & end dates", () => {
+        cy.get("[data-cy=start-date]")
+        .should("have.value", "2020-05-05")
+        cy.get("[data-cy=end-date")
+        .should("have.value", "2020-05-15")
     })
 })
