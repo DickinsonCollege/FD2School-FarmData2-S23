86890c9df9fb69f72d06a297dff433064afd21b8
udvale <enkhtaiu@dickinson.edu>
2023-04-15 15:58:26 -0400

    Tested the BarnKit tab contains the 3 sub-tabs

--- farmdata2/farmdata2_modules/fd2_barn_kit/cypress/barnkitTab.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/cypress/barnkitTab.spec.js
@@ -1,6 +1,11 @@
-describe("Testing Transplanting Report in BarnKit", () => {
+describe("Testing SubTabs in BarnKit", () => {
     beforeEach(() => {
         cy.login('manager1', 'farmdata2')
         cy.visit('/farm/fd2-barn-kit')
     })
+    it("BarnKit tab contains the subtabs" , () => {
+        cy.get(".pagination-sm").contains("Info").should("exist")
+        cy.get(".pagination-sm").contains("Seeding Report").should("exist")
+        cy.get(".pagination-sm").contains("Transplanting Report").should("exist")
+    }) 
 })
