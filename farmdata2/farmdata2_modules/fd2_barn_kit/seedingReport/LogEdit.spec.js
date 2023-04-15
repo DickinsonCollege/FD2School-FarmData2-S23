92d10c05d03c78dce1b54ddc711953b94d2053cc
sidlamsal <lamsals@dickinson.edu>
2023-04-14 23:41:00 -0400

    Edit log values

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/LogEdit.spec.js
+++
@@ -1,9 +0,0 @@
-describe('Test the Edit Button Behavior', () => {
-
-    beforeEach(() => {
-        cy.login('manager1', 'farmdata2')
-        cy.visit('/farm/fd2-field-kit/seedingReport')
-        cy.waitForPage()
-    }) 
-    
-})
