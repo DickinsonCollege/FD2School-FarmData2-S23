1fbf03bc703dae2fed6117cf52cf6897c6208daf
Melantha-Chen <yuxuan.chen.1031@outlook.com>
2023-04-17 09:18:54 -0400

    test for start date and end date and visibility of table modified

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.default.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.default.spec.js
@@ -1,14 +1,22 @@
-describe('eTest Seeding Report Default', () => {
+const dayjs = require('dayjs')
+
+describe('Test Seeding Report Default', () => {
   beforeEach(() => {
     cy.login("manager1", "farmdata2")
-    cy.visit("/farm/fd2_barn_kit/seedingReport")
+    cy.visit("/farm/fd2-barn-kit/seedingReport")
   })
 
-  // it("Test the default start date and end date", ()=>{
-  //   cy.get("[data-cy=date-range-selection]").click()
-  //   cy.get("[data-cy=date-range-selection]").should('have.value','2023-01-01')
-  //   cy.get("[data-cy=date-range-selection]").should('have.value','2023-04-05')
-  // })
+  it("Check start and end date with current year and date", () => {
+    cy.get("[data-cy = date-range-selection] > [data-cy = end-date-select] > [data-cy = date-select]")
+    .should('have.value',dayjs().format("YYYY-MM-DD"))
+    cy.get("[data-cy = date-range-selection] > [data-cy = start-date-select] > [data-cy = date-select]")
+    .should('have.value',dayjs().format("YYYY-01-01"))
+  })
+
+  it("The report table shouldn't invisible", () => {
+    cy.get("[data-cy = report-table]").should("not.exist")
+  })
+
   it("Check that the page contains header", () => {
     cy.get("[data-cy=text-center]").click()
     cy.get("[data-cy=text-center]").should("have text", "Seeding Report")
