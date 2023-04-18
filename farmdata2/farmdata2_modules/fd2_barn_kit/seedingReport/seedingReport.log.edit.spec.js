dc7981b0453b93bf49e05d2af7ecb0367199ec9b
EliasBerhe <eliastes123@gmail.com>
2023-04-18 18:32:24 -0400

    created context for direct seeding log test and added makeDirectSeeding function

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.log.edit.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.log.edit.spec.js
@@ -4,10 +4,11 @@
  * - The cancel button discards all edits and the log isn't changed
  * - For direct and tray seeding, edits are reflected in the table and database
  */
-
+const dayjs = require('dayjs')
 var FarmOSAPI = require("../../resources/FarmOSAPI.js")
 var getSessionToken = FarmOSAPI.getSessionToken
-
+var deleteRecord = FarmOSAPI.deleteRecord
+var createRecord = FarmOSAPI.createRecord
 describe('Test the Edit Button Behavior', () => {
     //session token to access database
     let sessionToken = null
@@ -22,7 +23,7 @@ describe('Test the Edit Button Behavior', () => {
             sessionToken = token
         })
         cy.visit('/farm/fd2-barn-kit/seedingReport')
-        cy.waitForPage()
+        //cy.waitForPage()
     }) 
 
     it("Check that cancel edit works", () => {
@@ -76,7 +77,100 @@ describe('Test the Edit Button Behavior', () => {
     })
 
 
+    context("Create a new log, do some tests, delete the log(s) ", () => {
 
-    
-    
+        let logID = null
+
+        beforeEach(() => {
+            cy.wrap(makeDirectSeeding("Test Seeding")).as("make-seeding")
+            
+
+            cy.get("@make-seeding")
+            .then((response) => {
+                logID = response.data.id            
+            })
+        })
+
+        
+        it("Check that save edit works for direct seeding.", () => {
+           
+            
+        })
+
+        
+        afterEach(() => {
+            cy.wrap(deleteRecord("/log/"+logID, sessionToken)).as("delete-seeding")
+            cy.get("@delete-seeding")
+        })
+    })
+
+    function makeDirectSeeding(name) {
+        let json = {
+            "name": name,
+            "type": "farm_seeding",
+            "timestamp": dayjs("1999-01-01").unix(),
+            "done": "1",  //any seeding recorded is done.
+            "notes": {
+                "value": "This is a test direct seeding",
+                "format": "farm_format"
+            },
+            "asset": [{ 
+                "id": "6",   //Associated planting
+                "resource": "farm_asset"
+            }],
+            "log_category": [{
+                "id": "240",
+                "resource": "taxonomy_term"
+            }],
+            "movement": {
+                "area": [{
+                    "id": "180",
+                    "resource": "taxonomy_term"
+                }]
+            },
+            "quantity": [
+                {
+                    "measure": "length", 
+                    "value": "10",  //total row feet
+                    "unit": {
+                        "id": "20", 
+                        "resource": "taxonomy_term"
+                    },
+                    "label": "Amount planted"
+                },
+                {
+                    "measure": "ratio", 
+                    "value": "20",
+                    "unit": {
+                        "id": "38",
+                        "resource": "taxonomy_term"
+                    },
+                    "label": "Rows/Bed"
+                },
+                {
+                    "measure": "time", 
+                    "value": "1.23", 
+                    "unit": {
+                        "id": "29",
+                        "resource": "taxonomy_term"
+                    },
+                    "label": "Labor"
+                },
+                {
+                    "measure": "count", 
+                    "value": "30", 
+                    "unit": {
+                        "id": "15",
+                        "resource": "taxonomy_term"
+                    },
+                    "label": "Workers"
+                },
+            ],
+            "created": dayjs().unix(),
+            "lot_number": "N/A (No Variety)",
+            "data": "{\"crop_tid\": \"161\"}"
+        }       
+
+        return createRecord('/log', json, sessionToken)
+    }
 })
