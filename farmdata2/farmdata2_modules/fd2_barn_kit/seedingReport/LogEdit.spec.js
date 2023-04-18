085a131e5534b9c1cda044747c8c1c8ce8d0d990
foogabob <foogabob@gmail.com>
2023-04-18 17:00:50 -0400

    addd description to the LogEdit file

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/LogEdit.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/LogEdit.spec.js
@@ -1,9 +1,12 @@
 describe('Test the Edit Button Behavior', () => {
-
+    /*
+    Tests the behavior of the log edit button.
+    */
     beforeEach(() => {
         cy.login('manager1', 'farmdata2')
         cy.visit('/farm/fd2-field-kit/seedingReport')
         cy.waitForPage()
     }) 
-    
+
+
 })
