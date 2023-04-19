2f0f7f19fc136f68c60ff189ed3255200ce60b3b
udvale <enkhtaiu@dickinson.edu>
2023-04-19 16:29:39 -0400

    Revised test for BarnKit subtabs ordering and length

--- farmdata2/farmdata2_modules/fd2_barn_kit/cypress/barnkitTab.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/cypress/barnkitTab.spec.js
@@ -8,4 +8,12 @@ describe("Testing SubTabs in BarnKit", () => {
         cy.get(".pagination-sm").contains("Seeding Report").should("exist")
         cy.get(".pagination-sm").contains("Transplanting Report").should("exist")
     }) 
+    it("BarnKit tabs are ordered properly" , () => {
+        cy.get(".pagination-sm").eq(0).should("contain", "Info")
+        cy.get(".pagination-sm > li > a").eq(1).should("contain", "Seeding Report")
+        cy.get(".pagination-sm > li > a").eq(2).should("contain.text", "Transplanting Report")
+    }) 
+    it("BarnKit has two sub tabs" , () => {
+        cy.get(".pagination-sm > li").should("have.length", "2") 
+    }) 
 })
