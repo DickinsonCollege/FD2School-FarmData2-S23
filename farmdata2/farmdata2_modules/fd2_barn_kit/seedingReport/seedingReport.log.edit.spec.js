764a751f535cda951042a982c71ccfa826c7f159
sidlamsal <lamsals@dickinson.edu>
2023-04-25 18:14:43 -0400

    removed commented code

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.log.edit.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.log.edit.spec.js
@@ -28,56 +28,6 @@ describe('Test the Edit Button Behavior', () => {
         //cy.waitForPage()
     }) 
 
-    /* it("Check that cancel edit works", () => {
-        //Select start date
-        cy.get('[data-cy="date-range-selection"] > [data-cy="start-date-select"] > [data-cy="date-select"]')
-            .type('2020-04-10')
-            .blur()
-    
-        //Click generate Report
-        cy.get('[data-cy="generate-rpt-btn"]').click()
-
-        //Get row date
-        cy.get('[data-cy="r0-Date"]').then(($div)=>{
-            //store original row date
-            const rowDate = $div.text()
-            
-            //get and click edit
-            cy.get('[data-cy="r0-edit-button"]').click()
-
-            //change date of row
-            cy.get('[data-cy="r0-Date-input"]')
-                .type('2020-04-10')
-            
-            //cancel
-            cy.get('[data-cy="r0-cancel-button"]')
-                .scrollIntoView()
-                .should('be.visible')
-                .click({force: true})
-            
-            //check that value is not changed in table
-            cy.get('[data-cy="r0-Date"]')
-                .should('have.text', rowDate)
-
-            //Check that the row is not updated in the database
-
-            //reload the page to reset the table
-            cy.reload()
-
-            //set the same date
-            cy.get('[data-cy="date-range-selection"] > [data-cy="start-date-select"] > [data-cy="date-select"]')
-            .type('2020-04-10')
-            .blur()
-
-            //get the same table
-            cy.get('[data-cy="generate-rpt-btn"]').click()
-
-            //check that the value is the same as the original 
-            cy.get('[data-cy="r0-Date"]')
-                .should('have.text', rowDate)
-        })
-    })
- */
     context("Create a new Direct seeding log, perform edit tests on it, then delete it. ", () => {
 
         let logID = null
@@ -146,6 +96,7 @@ describe('Test the Edit Button Behavior', () => {
             //change area of row
             cy.get('[data-cy="r0-Area-input"]')
                 .select('A')
+
             //cancel
             cy.get('[data-cy="r0-cancel-button"]')
                 .scrollIntoView()
