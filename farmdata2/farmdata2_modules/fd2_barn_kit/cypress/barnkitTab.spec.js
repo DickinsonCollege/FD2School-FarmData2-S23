23e0b4313cb82cb0355fde4238d699ab46ae510a
aliouas <aliouas@dickinson.edu>
2023-04-26 14:20:55 -0400

    Resolved issues and committed final changes

--- farmdata2/farmdata2_modules/fd2_barn_kit/cypress/barnkitTab.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/cypress/barnkitTab.spec.js
@@ -3,18 +3,20 @@ describe("Testing SubTabs in BarnKit", () => {
         cy.login('manager1', 'farmdata2')
         cy.visit('/farm/fd2-barn-kit')
     })
+
     it("BarnKit tab contains the subtabs" , () => {
         cy.get(".pagination-sm").contains("Info").should("exist")
         cy.get(".pagination-sm").contains("Seeding Report").should("exist")
         cy.get(".pagination-sm").contains("Transplanting Report").should("exist")
     }) 
+
     it("BarnKit tabs are ordered properly" , () => {
-        cy.get(".pagination-sm").eq(0).should("contain", "Info")
-        cy.get(".pagination-sm > li > a").eq(1).should("contain", "Seeding Report")
+        cy.get(".pagination-sm > li > a").eq(0).should("contain.text", "Info")
+        cy.get(".pagination-sm > li > a").eq(1).should("contain.text", "Seeding Report")
         cy.get(".pagination-sm > li > a").eq(2).should("contain.text", "Transplanting Report")
     }) 
-    it("BarnKit has two sub tabs" , () => {
-        cy.get(".pagination-sm > li").next().children().should("have.length", "2") 
-    }) 
 
+    it("BarnKit has three sub tabs" , () => {
+        cy.get(".pagination-sm > li").should("have.length", "3") 
+    }) 
 })
