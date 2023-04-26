444b84667fcf49486ab42299bf7baae883bd7811
Melantha-Chen <yuxuan.chen.1031@outlook.com>
2023-04-26 14:44:48 -0400

    "Made minor corrections to tests" > > Co-authored-by: Sai Atluri <atluris@dickinson.edu>

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.default.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.default.spec.js
@@ -4,25 +4,29 @@ describe('eTest Seeding Report Default', () => {
     cy.login("manager1", "farmdata2")
     cy.visit("/farm/fd2-barn-kit/seedingReport")
   })
+
   it("Check start and end date with current year", () => {
     cy.get("[data-cy = date-range-selection] > [data-cy = end-date-select] > [data-cy = date-select]")
       .should('have.value', dayjs().format("YYYY-MM-DD"))
     cy.get("[data-cy = date-range-selection] > [data-cy = start-date-select] > [data-cy = date-select]")
       .should('have.value', dayjs().format("YYYY-01-01"))
   })
+
   it("The report table shouldn't be visible", () => {
     cy.get("[data-cy = report-table]").should("not.exist")
+  }) 
 
-    it("Check that the page contains header", () => {
-      cy.get("[data-cy=report-header]").should("have text", "Seeding Report")
-    })
-    it("Test the generate button", () => {
-      cy.get('[data-cy=generate-rpt-btn]').should("have text", "Generate Report")
-      cy.get('[data-cy=generate-rpt-btn]').should("be.enabled")
+  it("Check that the page contains header", () => {
+    cy.get("[data-cy=report-header]").should("have.text", "Seeding Report")
+  })
 
-    })
-    it("Test to make sure that there is a section labeled Set Dates", () => {
-      cy.get("[data-cy=table-header]").should("have.text", "Set Dates")
-    })
+  it("Test the generate button", () => {
+    cy.get('[data-cy=generate-rpt-btn]').should("have.text", "Generate Report")
+    cy.get('[data-cy=generate-rpt-btn]').should("be.enabled")
+  })
+
+  it("Test to make sure that there is a section labeled Set Dates", () => {
+    cy.get("[data-cy=section-header]").should("have.text", "Set Dates")
   })
   
+})
