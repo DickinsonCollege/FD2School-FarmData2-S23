c9683910200961da1ffb8bee35788d4a8430c6ca
foogabob <foogabob@gmail.com>
2023-04-26 15:46:06 -0400

    changes to the sructure of the test

--- farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.submit.button.spec.js
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.submit.button.spec.js
@@ -18,238 +18,300 @@ describe('Test the submit button behavior', () => {
             .should("be.disabled")
     })
 
-    it('test tray seeding input has all required fields filled when submission button is enabled', () => {
-        //tests assume each of the buttons are 
-        //Select the tray seeding input type
-        cy.get('[data-cy="tray-seedings"]').click()
-            .blur()
+    context("Fills in every input field for Tray seeding, then perform tests", () => {
+        beforeEach(() => {
+            //Select a date
+            cy.get('[data-cy="date-selection"] > [data-cy="date-select"]').click()
+                .type('2021-04-10')
+                .blur()
 
-        //check that the submission button is disabled
-        cy.get('[data-cy="submit-button"]')
-            .should('be.disabled')
+            //select a crop
+            cy.get('[data-cy="crop-selection"] > [data-cy="dropdown-input"]')
+                .select('BEAN')
+                .blur()
 
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        //check the data section inputs alone don't enable the submit button
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            //Input workers 
+            cy.get('[data-cy="num-worker-input"] > [data-cy="text-input"]')
+                .type(3)
+                .blur()
 
-        //Select a date
-        cy.get('[data-cy="date-selection"] > [data-cy="date-select"]').click()
-            .type('2021-04-10')
-            .blur()
+            //Input minutes
+            cy.get('[data-cy="minute-input"] > [data-cy="text-input"]')
+                .type(30)
+                .blur()
 
-        //check that the submission button is disabled
-        cy.get('[data-cy="submit-button"]')
-            .should('be.disabled')
+            //Choose crop area
+            cy.get('[data-cy="tray-area-selection"] > [data-cy="dropdown-input"]')
+                .select('CHUAU')
+                .blur()
 
-        //select a crop
-        cy.get('[data-cy="crop-selection"] > [data-cy="dropdown-input"]')
-            .select('BEAN')
-            .blur()
+            //check that the submission button is disabled
+            cy.get('[data-cy="submit-button"]')
+                .should('be.disabled')
 
-        //check that the submission button is disabled
-        cy.get('[data-cy="submit-button"]')
-            .should('be.disabled')
-        
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        //check the labor section inputs alone don't enable the submit button
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            //Input Cells/Tray
+            cy.get('[data-cy="num-cell-input"] > [data-cy="text-input"]')
+                .type(5)
+                .blur()
+                
+            //check that the submission button is disabled
+            cy.get('[data-cy="submit-button"]')
+                .should('be.disabled')
 
-        //Input workers 
-        cy.get('[data-cy="num-worker-input"] > [data-cy="text-input"]')
-            .type(3)
-            .blur()
+            //Input Trays
+            cy.get('[data-cy="num-tray-input"] > [data-cy="text-input"]')
+                .type(5)
+                .blur()
 
-        //check that the submission button is disabled
-        cy.get('[data-cy="submit-button"]')
-            .should('be.disabled')
+            //check that the submission button is disabled
+            cy.get('[data-cy="submit-button"]')
+                .should('be.disabled')
 
-        //Input minutes
-        cy.get('[data-cy="minute-input"] > [data-cy="text-input"]')
-            .type(30)
-            .blur()
-            
-        //check that the submission button is disabled
-        cy.get('[data-cy="submit-button"]')
-            .should('be.disabled')
+            //Input Seeds
+            cy.get('[data-cy="num-seed-input"] > [data-cy="text-input"]')
+                .type(100)
+                .blur()
+        }) 
 
-        //Switch time units to hours
-        cy.get('[data-cy="time-unit"] > [data-cy="dropdown-input"]')
-            .select('hours')
-            .blur()
+        it('Test submit button is enabled', () => {
+            //all blank fields have now been filled, the submission button should be enabled
+            cy.get('[data-cy="submit-button"]')
+                .should('not.be.disabled')
+        })
 
-        //Input hours
-        cy.get('[data-cy="hour-input"] > [data-cy="text-input"]')
-            .type(3)
-            .blur()
+        it('test changing units to hours disables the submit button', () => {
+            //Switch time units to hours
+            cy.get('[data-cy="time-unit"] > [data-cy="dropdown-input"]')
+                .select('hours')
+                .blur()
 
-        //check that the submission button is disabled
-        cy.get('[data-cy="submit-button"]')
-            .should('be.disabled')
+            //Input hours
+            cy.get('[data-cy="hour-input"] > [data-cy="text-input"]')
+                .type(3)
+                .blur()
 
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        //check the seeding section tray inputs don't enable the submit button
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            //the submission button should be enabled
+            cy.get('[data-cy="submit-button"]')
+                .should('not.be.disabled')
+        })
 
-        //Choose crop area
-        cy.get('[data-cy="tray-area-selection"] > [data-cy="dropdown-input"]')
-            .select('CHUAU')
-            .blur()
+        it('test changing units to hours disables the submit button', () => {
+            //Switch time units to hours
+            cy.get('[data-cy="time-unit"] > [data-cy="dropdown-input"]')
+                .select('hours')
+                .blur()
 
-        //check that the submission button is disabled
-        cy.get('[data-cy="submit-button"]')
-            .should('be.disabled')
+            //Input hours
+            cy.get('[data-cy="hour-input"] > [data-cy="text-input"]')
+                .type(3)
+                .blur()
 
-        //Input Cells/Tray
-        cy.get('[data-cy="num-cell-input"] > [data-cy="text-input"]')
-            .type(5)
-            .blur()
-            
-        //check that the submission button is disabled
-        cy.get('[data-cy="submit-button"]')
-            .should('be.disabled')
-
-        //Input Trays
-        cy.get('[data-cy="num-tray-input"] > [data-cy="text-input"]')
-            .type(5)
-            .blur()
-
-        //check that the submission button is disabled
-        cy.get('[data-cy="submit-button"]')
-            .should('be.disabled')
-
-        //Input Seeds
-        cy.get('[data-cy="num-seed-input"] > [data-cy="text-input"]')
-            .type(100)
-            .blur()
-
-        //all blank fields have now been filled submission button should be enabled
-        //check that the submission button is enabled
-        cy.get('[data-cy="submit-button"]')
-            .should('not.be.disabled')
+            //the submission button should be enabled
+            cy.get('[data-cy="submit-button"]')
+                .should('not.be.disabled')
+        })
     })
 
+//     it('test tray seeding input has all required fields filled when submission button is enabled', () => {
+//         //Select the tray seeding input type
+//         cy.get('[data-cy="tray-seedings"]').click()
+//             .blur()
 
-    it('test Direct seeding input has all required fields filled when submission button is enabled', () => {
+//         //check that the submission button is disabled
+//         cy.get('[data-cy="submit-button"]')
+//             .should('be.disabled')
 
-        //Select the direct seeding input type
-        cy.get('[data-cy="direct-seedings"]').click()
-            .blur()
+//         //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//         //fill every input field
+//         //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-        //check that the submission button is disabled
-        cy.get('[data-cy="submit-button"]')
-            .should('be.disabled')
+//         //Select a date
+//         cy.get('[data-cy="date-selection"] > [data-cy="date-select"]').click()
+//             .type('2021-04-10')
+//             .blur()
 
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        //check the direct seeding section  inputs don't enable the submit button
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//         //select a crop
+//         cy.get('[data-cy="crop-selection"] > [data-cy="dropdown-input"]')
+//             .select('BEAN')
+//             .blur()
 
-        //Choose crop area
-        cy.get('[data-cy="direct-area-selection"] > [data-cy="dropdown-input"]')
-            .select('GHANA-1')
-            .blur()
+//         //Input workers 
+//         cy.get('[data-cy="num-worker-input"] > [data-cy="text-input"]')
+//             .type(3)
+//             .blur()
 
-        //check that the submission button is disabled
-        cy.get('[data-cy="submit-button"]')
-            .should('be.disabled')
+//         //Input minutes
+//         cy.get('[data-cy="minute-input"] > [data-cy="text-input"]')
+//             .type(30)
+//             .blur()
 
-        //Input Rows/Bed
-        cy.get('[data-cy="num-rowbed-input"] > [data-cy="text-input"]')
-            .type(5)
-            .blur()
+//         //Switch time units to hours
+//         cy.get('[data-cy="time-unit"] > [data-cy="dropdown-input"]')
+//             .select('hours')
+//             .blur()
 
-        //check that the submission button is disabled
-        cy.get('[data-cy="submit-button"]')
-            .should('be.disabled')
+//         //Input hours
+//         cy.get('[data-cy="hour-input"] > [data-cy="text-input"]')
+//             .type(3)
+//             .blur()
 
-        // Select Bed Feet
-        cy.get('[data-cy="unit-feet"] > [data-cy="dropdown-input"]')
-            .select('Bed Feet')
-            .blur()
+//         //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//         //check the seeding section tray inputs don't enable the submit button
+//         //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-        //Input Bed Feet
-        cy.get('[data-cy="num-feet-input"] > [data-cy="text-input"]')
-            .type(3)
-            .blur()
-        //check that the submission button is disabled
-        cy.get('[data-cy="submit-button"]')
-            .should('be.disabled')
+//         //Choose crop area
+//         cy.get('[data-cy="tray-area-selection"] > [data-cy="dropdown-input"]')
+//             .select('CHUAU')
+//             .blur()
 
-        
-       // Select Row Feet
-       cy.get('[data-cy="unit-feet"] > [data-cy="dropdown-input"]')
-            .select('Row Feet')
-            .blur()
+//         //check that the submission button is disabled
+//         cy.get('[data-cy="submit-button"]')
+//             .should('be.disabled')
 
-        //Input Row Feet
-        cy.get('[data-cy="num-feet-input"] > [data-cy="text-input"]')
-            .type(2)
-            .blur()
-        //check that the submission button is disabled
-        cy.get('[data-cy="submit-button"]')
-            .should('be.disabled')
-
-         //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        //check the data section inputs alone don't enable the submit button
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-        //Select a date
-        cy.get('[data-cy="date-selection"] > [data-cy="date-select"]').click()
-            .type('2021-04-10')
-            .blur()
-
-        //check that the submission button is disabled
-        cy.get('[data-cy="submit-button"]')
-            .should('be.disabled')
-
-        //select a crop
-        cy.get('[data-cy="crop-selection"] > [data-cy="dropdown-input"]')
-            .select('BEAN')
-            .blur()
-
-        //check that the submission button is disabled
-        cy.get('[data-cy="submit-button"]')
-            .should('be.disabled')
-        
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        //check the labor section inputs alone don't enable the submit button
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-        //Input workers 
-        cy.get('[data-cy="num-worker-input"] > [data-cy="text-input"]')
-            .type(3)
-            .blur()
-
-        //check that the submission button is disabled
-        cy.get('[data-cy="submit-button"]')
-            .should('be.disabled')
-
-        //Input minutes
-        cy.get('[data-cy="minute-input"] > [data-cy="text-input"]')
-            .type(30)
-            .blur()
+//         //Input Cells/Tray
+//         cy.get('[data-cy="num-cell-input"] > [data-cy="text-input"]')
+//             .type(5)
+//             .blur()
             
-        //check that the submission button is disabled
-        cy.get('[data-cy="submit-button"]')
-            .should('be.disabled')
+//         //check that the submission button is disabled
+//         cy.get('[data-cy="submit-button"]')
+//             .should('be.disabled')
 
-        //Switch time units to hours
-        cy.get('[data-cy="time-unit"] > [data-cy="dropdown-input"]')
-            .select('hours')
-            .blur()
+//         //Input Trays
+//         cy.get('[data-cy="num-tray-input"] > [data-cy="text-input"]')
+//             .type(5)
+//             .blur()
 
-        //Input hours
-        cy.get('[data-cy="hour-input"] > [data-cy="text-input"]')
-            .type(3)
-            .blur()
+//         //check that the submission button is disabled
+//         cy.get('[data-cy="submit-button"]')
+//             .should('be.disabled')
 
-        //check that the submission button is disabled
-        cy.get('[data-cy="submit-button"]')
-            .should('be.disabled')
+//         //Input Seeds
+//         cy.get('[data-cy="num-seed-input"] > [data-cy="text-input"]')
+//             .type(100)
+//             .blur()
 
-        //all blank fields have now been filled submission button should be enabled
-        //check that the submission button is enabled
-        cy.get('[data-cy="submit-button"]')
-            .should('not.be.disabled')
-    })
+//         //all blank fields have now been filled submission button should be enabled
+//         //check that the submission button is enabled
+//         cy.get('[data-cy="submit-button"]')
+//             .should('not.be.disabled')
+//     })
 
+//     it('test Direct seeding input has all required fields filled when submission button is enabled', () => {
+
+//         //Select the direct seeding input type
+//         cy.get('[data-cy="direct-seedings"]').click()
+//             .blur()
+
+//         //check that the submission button is disabled
+//         cy.get('[data-cy="submit-button"]')
+//             .should('be.disabled')
+
+//         //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//         //check the direct seeding section  inputs don't enable the submit button
+//         //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//         //Choose crop area
+//         cy.get('[data-cy="direct-area-selection"] > [data-cy="dropdown-input"]')
+//             .select('GHANA-1')
+//             .blur()
+
+//         //check that the submission button is disabled
+//         cy.get('[data-cy="submit-button"]')
+//             .should('be.disabled')
+
+//         //Input Rows/Bed
+//         cy.get('[data-cy="num-rowbed-input"] > [data-cy="text-input"]')
+//             .type(5)
+//             .blur()
+
+//         //check that the submission button is disabled
+//         cy.get('[data-cy="submit-button"]')
+//             .should('be.disabled')
+
+//         // Select Bed Feet
+//         cy.get('[data-cy="unit-feet"] > [data-cy="dropdown-input"]')
+//             .select('Bed Feet')
+//             .blur()
+
+//         //Input Bed Feet
+//         cy.get('[data-cy="num-feet-input"] > [data-cy="text-input"]')
+//             .type(3)
+//             .blur()
+//         //check that the submission button is disabled
+//         cy.get('[data-cy="submit-button"]')
+//             .should('be.disabled')
+        
+//        // Select Row Feet
+//        cy.get('[data-cy="unit-feet"] > [data-cy="dropdown-input"]')
+//             .select('Row Feet')
+//             .blur()
+
+//         //Input Row Feet
+//         cy.get('[data-cy="num-feet-input"] > [data-cy="text-input"]')
+//             .type(2)
+//             .blur()
+//         //check that the submission button is disabled
+//         cy.get('[data-cy="submit-button"]')
+//             .should('be.disabled')
+
+//          //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//         //check the data section inputs alone don't enable the submit button
+//         //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//         //Select a date
+//         cy.get('[data-cy="date-selection"] > [data-cy="date-select"]').click()
+//             .type('2021-04-10')
+//             .blur()
+
+//         //check that the submission button is disabled
+//         cy.get('[data-cy="submit-button"]')
+//             .should('be.disabled')
+
+//         //select a crop
+//         cy.get('[data-cy="crop-selection"] > [data-cy="dropdown-input"]')
+//             .select('BEAN')
+//             .blur()
+
+//         //check that the submission button is disabled
+//         cy.get('[data-cy="submit-button"]')
+//             .should('be.disabled')
+        
+//         //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//         //check the labor section inputs alone don't enable the submit button
+//         //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//         //Input workers 
+//         cy.get('[data-cy="num-worker-input"] > [data-cy="text-input"]')
+//             .type(3)
+//             .blur()
+
+//         //check that the submission button is disabled
+//         cy.get('[data-cy="submit-button"]')
+//             .should('be.disabled')
+
+//         //Input minutes
+//         cy.get('[data-cy="minute-input"] > [data-cy="text-input"]')
+//             .type(30)
+//             .blur()
+            
+//         //check that the submission button is disabled
+//         cy.get('[data-cy="submit-button"]')
+//             .should('be.disabled')
+
+//         //Switch time units to hours
+//         cy.get('[data-cy="time-unit"] > [data-cy="dropdown-input"]')
+//             .select('hours')
+//             .blur()
+
+//         //Input hours
+//         cy.get('[data-cy="hour-input"] > [data-cy="text-input"]')
+//             .type(3)
+//             .blur()
+
+//         //all blank fields have now been filled submission button should be enabled
+//         //check that the submission button is enabled
+//         cy.get('[data-cy="submit-button"]')
+//             .should('not.be.disabled')
+//     })
 })
