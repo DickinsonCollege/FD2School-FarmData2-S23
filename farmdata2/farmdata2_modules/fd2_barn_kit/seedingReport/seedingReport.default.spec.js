52729a2989f8340418fcad9a4ffac7e190775e5e
saiatl2354 <atluris@dickinson.edu>
2023-04-26 11:40:19 -0400

    Updated tests

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.default.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.default.spec.js
@@ -9,16 +9,14 @@ describe('eTest Seeding Report Default', () => {
     //   cy.get("[data-cy=date-range-selection]").should('have.value','2023-04-05')
     // })
     it("Check that the page contains header", () => {
-      cy.get("[data-cy=text-center]").click()
-      cy.get("[data-cy=text-center]").should("have text", "Seeding Report")
+      cy.get("[data-cy=report-header]").should("have text", "Seeding Report")
     })
     it("Test the generate button", () => {
-      cy.get('[data-cy=generate-rpt-btn]').click()
-      cy.get('[data-cy=generate-rpt-btn]').should('exist')
+      cy.get('[data-cy=generate-rpt-btn]').should("have text", "Generate Report")
+      cy.get('[data-cy=generate-rpt-btn]').should("be.enabled")
   
     })
-    it("Test to make sure that there are dates", () => {
-      cy.get("[data-cy=farm]").should("have.text", "Set Dates")
-      cy.get("[data-cy=panel-heading]").should("have.text", "Set Dates")
+    it("Test to make sure that there is a section labeled Set Dates", () => {
+      cy.get("[data-cy=table-header]").should("have.text", "Set Dates")
     })
   })
