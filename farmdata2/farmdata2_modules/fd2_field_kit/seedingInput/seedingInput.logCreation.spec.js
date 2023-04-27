dbd545743cbc285d045d2177892e310b71998e69
won369369 <wj0293@gmail.com>
2023-04-26 21:13:39 -0400

    Add Cypress tests for Direct and Tray Seeding log creation and validation

--- farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.logCreation.spec.js
+++ farmdata2/farmdata2_modules/fd2_field_kit/seedingInput/seedingInput.logCreation.spec.js
@@ -1,5 +1,12 @@
 const dayjs = require('dayjs')
-var FarmOSAPI = require("../../resources/FarmOSAPI.js")
+
+var FarmOSAPI = require('../../resources/FarmOSAPI.js')
+
+var getRecord = FarmOSAPI.getRecord
+var getSessionToken = FarmOSAPI.getSessionToken
+var deleteRecord = FarmOSAPI.deleteRecord
+var createRecord = FarmOSAPI.createRecord
+
 var getAllPages = FarmOSAPI.getAllPages
 
 describe('Tests for SeedingInput log creation', () => {
@@ -134,7 +141,7 @@ describe('Tests for SeedingInput log creation', () => {
       .click()
     cy.get('[data-cy=cancel-button]')
       .click()
-      
+
     //checks if the "Submit" button is enabled
     cy.get('[data-cy=submit-button]')
       .should('not.be.disabled')
@@ -144,15 +151,15 @@ describe('Tests for SeedingInput log creation', () => {
   it("Checks no log was created in the database when the “Cancel” button is clicked", () => {
 
     // Get the start and end timestamps for the date range we want.
-    let start = dayjs("2020-05-05","YYYY-MM-DD").unix()
+    let start = dayjs("2020-05-05", "YYYY-MM-DD").unix()
     // Add 1 day here to get to the end of May 15th.
-    let end = dayjs("2020-05-15","YYYY-MM-DD").add(1,'day').unix()
+    let end = dayjs("2020-05-15", "YYYY-MM-DD").add(1, 'day').unix()
 
     /*
      * Request all of the farm seedings between 2020-05-05 and 2020-05-15. There
      * were 56 seeding in this date range.
      */
-    let url = "/log.json?type=farm_seeding&timestamp[gt]="+start+"&timestamp[lt]="+end
+    let url = "/log.json?type=farm_seeding&timestamp[gt]=" + start + "&timestamp[lt]=" + end
     let seedingLogsBefore = []
     cy.wrap(getAllPages(url, seedingLogsBefore)).as("get-logs")
 
@@ -160,9 +167,9 @@ describe('Tests for SeedingInput log creation', () => {
      * Wait for the promise returned from getAllPages to resolve.
      */
     cy.get("@get-logs")
-    .then((response) => {
-      expect(seedingLogsBefore.length).to.equal(56)
-    })
+      .then((response) => {
+        expect(seedingLogsBefore.length).to.equal(56)
+      })
 
     //type all inputs into seedingInput report
     cy.get('[data-cy=date-select')
@@ -200,8 +207,227 @@ describe('Tests for SeedingInput log creation', () => {
      * Wait for the promise returned from getAllPages to resolve.
      */
     cy.get("@get-logs-again")
-    .then((response) => {
-      expect(seedingLogsAfter.length).to.equal(56)
-    })
+      .then((response) => {
+        expect(seedingLogsAfter.length).to.equal(56)
+      })
   })
 })
+
+describe("Test if 'Submit' button creates a new seeding log in the database", () => {
+
+  let sessionToken = null;
+  let logID = null;
+
+  beforeEach(() => {
+    cy.login("manager1", "farmdata2")
+      .then(() => {
+        return cy.wrap(getSessionToken());
+      })
+      .then((token) => {
+        sessionToken = token;
+        cy.visit('/farm/fd2-field-kit/seedingInput');
+      });
+  });
+
+  //sub-task #5 issue #203
+  context("Create and check new Direct Seeding log", () => {
+    beforeEach(() => {
+      cy.wrap(makeDirectSeeding("Test Direct Seeding"))
+        .then((response) => {
+          logID = response.data.id;
+        });
+    });
+
+    it("Direct Seeding log is created and found in the database", () => {
+      cy.wrap(getRecord(`/log.json?id=${logID}`))
+        .then((response) => {
+          expect(response.data.list[0].name).to.equal("Test Direct Seeding");
+          expect(response.data.list[0].type).to.equal("farm_seeding");
+          expect(response.data.list[0].log_category[0].name).to.equal("Direct Seedings");
+        });
+    });
+
+    afterEach(() => {
+      cy.wrap(deleteRecord(`/log/${logID}`, sessionToken));
+    });
+  });
+
+  //sub-task #6 issue #203
+  context("Create and check new Tray Seeding log", () => {
+    beforeEach(() => {
+      cy.wrap(makeTraySeeding("Test Tray Seeding"))
+        .then((response) => {
+          logID = response.data.id;
+        });
+    });
+
+    it("Tray Seeding log is created and found in the database", () => {
+      cy.wrap(getRecord(`/log.json?id=${logID}`))
+        .then((response) => {
+          expect(response.data.list[0].name).to.equal("Test Tray Seeding");
+          expect(response.data.list[0].type).to.equal("farm_seeding");
+          expect(response.data.list[0].log_category[0].name).to.equal("Tray Seedings");
+        });
+    });
+
+    afterEach(() => {
+      cy.wrap(deleteRecord(`/log/${logID}`, sessionToken));
+    });
+  });
+
+  /**
+* This function will return a promise that creates a new Direct Seeding log.
+*/
+
+  function makeDirectSeeding(name) {
+    let json = {
+      "name": name,
+      "type": "farm_seeding",
+      "timestamp": dayjs("1999-01-01").unix(),
+      "done": "1",  //any seeding recorded is done.
+      "notes": {
+        "value": "This is a test direct seeding",
+        "format": "farm_format"
+      },
+      "asset": [{
+        "id": "6",   //Associated planting
+        "resource": "farm_asset"
+      }],
+      "log_category": [{
+        "id": "240",
+        "resource": "taxonomy_term"
+      }],
+      "movement": {
+        "area": [{
+          "id": "180",
+          "resource": "taxonomy_term"
+        }]
+      },
+      "quantity": [
+        {
+          "measure": "length",
+          "value": "10",  //total row feet
+          "unit": {
+            "id": "20",
+            "resource": "taxonomy_term"
+          },
+          "label": "Amount planted"
+        },
+        {
+          "measure": "ratio",
+          "value": "20",
+          "unit": {
+            "id": "38",
+            "resource": "taxonomy_term"
+          },
+          "label": "Rows/Bed"
+        },
+        {
+          "measure": "time",
+          "value": "1.23",
+          "unit": {
+            "id": "29",
+            "resource": "taxonomy_term"
+          },
+          "label": "Labor"
+        },
+        {
+          "measure": "count",
+          "value": "30",
+          "unit": {
+            "id": "15",
+            "resource": "taxonomy_term"
+          },
+          "label": "Workers"
+        },
+      ],
+      "created": dayjs().unix(),
+      "lot_number": "N/A (No Variety)",
+      "data": "{\"crop_tid\": \"161\"}"
+    }
+
+    return createRecord('/log', json, sessionToken)
+  }
+
+  /**
+  * This function will return a promise that creates a new Tray Seeding log.
+  */
+  function makeTraySeeding(name) {
+    let json = {
+      "name": name,
+      "type": "farm_seeding",
+      "timestamp": dayjs("1999-01-01").unix(),
+      "done": "1",  //any seeding recorded is done.
+      "notes": {
+        "value": "This is a test tray seeding",
+        "format": "farm_format"
+      },
+      "asset": [{
+        "id": "6",   //Associated planting
+        "resource": "farm_asset"
+      }],
+      "log_category": [{
+        "id": "241",
+        "resource": "taxonomy_term"
+      }],
+      "movement": {
+        "area": [{
+          "id": "180",
+          "resource": "taxonomy_term"
+        }]
+      },
+      "quantity": [
+        {
+          "measure": "count",
+          "value": "10",  //number of seed planted
+          "unit": {
+            "id": "17",
+            "resource": "taxonomy_term"
+          },
+          "label": "Seeds planted"
+        },
+        {
+          "measure": "count",
+          "value": "20",  //number of flats(trays)
+          "unit": {
+            "id": "12",
+            "resource": "taxonomy_term"
+          },
+          "label": "Flats used"
+        },
+        {
+          "measure": "ratio",
+          "value": "30",  //cells per flat
+          "unit": {
+            "id": "37",
+            "resource": "taxonomy_term"
+          },
+          "label": "Cells/Flat"
+        },
+        {
+          "measure": "time",
+          "value": "1.23",  //hours worked
+          "unit": {
+            "id": "29",
+            "resource": "taxonomy_term"
+          },
+          "label": "Labor"
+        },
+        {
+          "measure": "count",
+          "value": "40",
+          "unit": {
+            "id": "15",
+            "resource": "taxonomy_term"
+          },
+          "label": "Workers"
+        },
+      ],
+      "created": dayjs().unix(),
+      "lot_number": "N/A (No Variety)",
+      "data": "{\"crop_tid\": \"161\"}"
+    }
+
+    return createRecord('/log', json, sessionToken)
+  }
+});
