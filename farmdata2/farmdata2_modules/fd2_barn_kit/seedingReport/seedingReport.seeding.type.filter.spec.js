d099bd12eb13bb336a55446d6adda316b5ca312a
saiatl2354 <atluris@dickinson.edu>
2023-05-01 12:48:42 -0400

    test updates

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.seeding.type.filter.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.seeding.type.filter.spec.js
@@ -1,15 +1,19 @@
 describe('Testing the seeding type filter', () => {
     beforeEach(() => {
-      cy.login("manager1", "farmdata2")
-      cy.visit("/farm/fd2_barn_kit/seedingReport")
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2_barn_kit/seedingReport")
     })
-it("Tests that direct and tray seedings are both shown when All is selected", () => {
+    it("Tests that direct and tray seedings are both shown when All is selected", () => {
         cy.get('[data-cy=generate-rpt-btn]').click()
-        cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]').select('direct seeding')
-        cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]').should('have.value', 'direct seeding’)
- cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]').select('table seeding')
-        cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]').should('table', 'direct seeding’)
-           })
-       cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]').should('have.value', 'All')
+        cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]').select('direct seeding').should('have.value', 'direct seeding')
+        cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]').should('have.value', 'All')
+
+        it("Checks the direct seeding", () => {
+            cy.get('[data-cy=crop-dropdown] > [data-cy=dropdown-input]').select('table seeding').should('table', 'direct seeding')
         })
-    
+
+    })
+
+})
+
+
