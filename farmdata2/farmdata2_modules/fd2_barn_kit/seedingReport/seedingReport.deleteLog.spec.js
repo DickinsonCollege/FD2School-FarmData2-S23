c1092f3ea204ad3c4efe0b4cec5d08e9f2c3aa96
udvale <enkhtaiu@dickinson.edu>
2023-05-06 23:54:27 -0400

    Added new logs and test for deleting 1 log

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.deleteLog.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.deleteLog.spec.js
@@ -22,36 +22,64 @@ describe('Testing if logs are properly removed from the database', () => {
         cy.waitForPage()
     })
 
-    context("Create a new log, delete the log(s)", () => {
+    context("Create a new logs, delete the log(s)", () => {
         let logID = null
 
-        beforeEach(() => {
-            cy.wrap(makeDirectSeeding("Test Seeding")).as("make-seeding")
-            cy.get("@make-seeding")
-            .then((response) => {
-                logID = response.data.id            
+        context("Adding logs, dates and testing for singular seeding log", () => {
+            //Create new logs for testing and add dates
+            beforeEach(() => {
+                cy.wrap(makeDirectSeeding("Test Seeding")).as("make-seeding")
+                cy.get("@make-seeding")
+                .then((response) => {
+                    logID = response.data.id            
+                })
+
+                cy.get('[data-cy=start-date-select]')
+                    .should("exist")
+                    .type('2023-05-01')
+                cy.get('[data-cy=end-date-select]')
+                    .should("exist")
+                    .type('2023-05-04')
+                cy.get('[data-cy=generate-rpt-btn]')
+                    .click()
+                cy.waitForPage()
             })
-            cy.get('[data-cy=start-date-select]').type('1999-01-01')
-            cy.get('[data-cy=end-date-select]').type('1999-01-04')
-            cy.get('[data-cy=generate-rpt-btn]').click()
+
+            it("Delete a singular seeding log from the row.", () => {
+                cy.get('[data-cy=report-table]').should('exist')
+                cy.get("[data-cy = r1-cbuttonCheckbox]").click()
+                cy.get("[data-cy = delete-button]")
+                    .click((response) => {
+                        expect(response.status).to.equal(200)
+                    })
+                expect(true).to.equal(true)
+            })
+
+            //Delete the created logs so the database is refreshed
+            afterEach(() => {
+                cy.wrap(deleteRecord("/log/"+logID, sessionToken)).as("delete-seeding")
+                cy.get("@delete-seeding")
+            })
         })
 
-        it("Test to delete a singular seeding log.", () => {
-          cy.wrap(getRecord("/log.json?id=6")).as("get-log")
-          cy.get("[data-cy = r0-cbuttonCheckbox]").click()
-          cy.get("[data-cy = delete-button]").click()
-            expect(true).to.equal(true)
-        })
+        
 
-        it("Test to delete a multiple seeding logs.", () => {
-        //   cy.wrap(getRecord("/log.json?id=6")).as("get-log")
-          cy.get("[data-cy = r0-cbuttonCheckbox]").click()
-          cy.get("[data-cy = r1-cbuttonCheckbox]").click()
-          cy.get("[data-cy = r2-cbuttonCheckbox]").click()
-          cy.get("[data-cy = r5-cbuttonCheckbox]").click()
-          cy.get("[data-cy = delete-button]").click()
-            expect(true).to.equal(true)
-        })
+        // it("Test to delete a singular seeding log.", () => {
+        // //   cy.wrap(getRecord("/log.json?id=6")).as("get-log")
+        //   cy.get("[data-cy = r0-cbuttonCheckbox]").click()
+        //   cy.get("[data-cy = delete-button]").click()
+        //     expect(true).to.equal(true)
+        // })
+
+        // it("Test to delete a multiple seeding logs.", () => {
+        // //   cy.wrap(getRecord("/log.json?id=6")).as("get-log")
+        //   cy.get("[data-cy = r0-cbuttonCheckbox]").click()
+        //   cy.get("[data-cy = r1-cbuttonCheckbox]").click()
+        //   cy.get("[data-cy = r2-cbuttonCheckbox]").click()
+        //   cy.get("[data-cy = r5-cbuttonCheckbox]").click()
+        //   cy.get("[data-cy = delete-button]").click()
+        //     expect(true).to.equal(true)
+        // })
 
         // it("Test to cancel the deletion of a seeding log(s).", () => {
         //   cy.wrap(getRecord("/log.json?id=6")).as("get-log")
@@ -76,7 +104,7 @@ describe('Testing if logs are properly removed from the database', () => {
         let json = {
             "name": name,
             "type": "farm_seeding",
-            "timestamp": dayjs("1999-01-01").unix(),
+            "timestamp": dayjs("2023-05-03").unix(),
             "done": "1",  //any seeding recorded is done.
             "notes": {
                 "value": "This is a test direct seeding",
@@ -135,7 +163,147 @@ describe('Testing if logs are properly removed from the database', () => {
                 },
             ],
             "created": dayjs().unix(),
-            "lot_number": "N/A (No Variety)",
+            "lot_number": "BEAN",
+            "data": "{\"crop_tid\": \"161\"}"
+        }       
+
+        return createRecord('/log', json, sessionToken)
+    }
+
+    function makeDirectSeeding(name) {
+        let json = {
+            "name": name,
+            "type": "farm_seeding",
+            "timestamp": dayjs("2023-05-01").unix(),
+            "done": "1",  //any seeding recorded is done.
+            "notes": {
+                "value": "This is a test direct seeding",
+                "format": "farm_format"
+            },
+            "asset": [{ 
+                "id": "2",   //Associated planting
+                "resource": "farm_asset"
+            }],
+            "log_category": [{
+                "id": "200",
+                "resource": "taxonomy_term"
+            }],
+            "movement": {
+                "area": [{
+                    "id": "100",
+                    "resource": "taxonomy_term"
+                }]
+            },
+            "quantity": [
+                {
+                    "measure": "length", 
+                    "value": "20",  //total row feet
+                    "unit": {
+                        "id": "30", 
+                        "resource": "taxonomy_term"
+                    },
+                    "label": "Amount planted"
+                },
+                {
+                    "measure": "ratio", 
+                    "value": "20",
+                    "unit": {
+                        "id": "38",
+                        "resource": "taxonomy_term"
+                    },
+                    "label": "Rows/Bed"
+                },
+                {
+                    "measure": "time", 
+                    "value": "1.23", 
+                    "unit": {
+                        "id": "29",
+                        "resource": "taxonomy_term"
+                    },
+                    "label": "Labor"
+                },
+                {
+                    "measure": "count", 
+                    "value": "35", 
+                    "unit": {
+                        "id": "20",
+                        "resource": "taxonomy_term"
+                    },
+                    "label": "Workers"
+                },
+            ],
+            "created": dayjs().unix(),
+            "lot_number": "ARUGULA",
+            "data": "{\"crop_tid\": \"161\"}"
+        }       
+
+        return createRecord('/log', json, sessionToken)
+    }
+
+    function makeDirectSeeding(name) {
+        let json = {
+            "name": name,
+            "type": "farm_seeding",
+            "timestamp": dayjs("2023-05-03").unix(),
+            "done": "1",  //any seeding recorded is done.
+            "notes": {
+                "value": "This is a test direct seeding",
+                "format": "farm_format"
+            },
+            "asset": [{ 
+                "id": "6",   //Associated planting
+                "resource": "farm_asset"
+            }],
+            "log_category": [{
+                "id": "240",
+                "resource": "taxonomy_term"
+            }],
+            "movement": {
+                "area": [{
+                    "id": "180",
+                    "resource": "taxonomy_term"
+                }]
+            },
+            "quantity": [
+                {
+                    "measure": "length", 
+                    "value": "10",  //total row feet
+                    "unit": {
+                        "id": "20", 
+                        "resource": "taxonomy_term"
+                    },
+                    "label": "Amount planted"
+                },
+                {
+                    "measure": "ratio", 
+                    "value": "20",
+                    "unit": {
+                        "id": "38",
+                        "resource": "taxonomy_term"
+                    },
+                    "label": "Rows/Bed"
+                },
+                {
+                    "measure": "time", 
+                    "value": "1.23", 
+                    "unit": {
+                        "id": "29",
+                        "resource": "taxonomy_term"
+                    },
+                    "label": "Labor"
+                },
+                {
+                    "measure": "count", 
+                    "value": "30", 
+                    "unit": {
+                        "id": "15",
+                        "resource": "taxonomy_term"
+                    },
+                    "label": "Workers"
+                },
+            ],
+            "created": dayjs().unix(),
+            "lot_number": "BROCCOLI",
             "data": "{\"crop_tid\": \"161\"}"
         }       
 
@@ -149,7 +317,7 @@ describe('Testing if logs are properly removed from the database', () => {
         let json = {
             "name": name,
             "type": "farm_seeding",
-            "timestamp": dayjs("1999-01-01").unix(),
+            "timestamp": dayjs("2023-05-04").unix(),
             "done": "1",  //any seeding recorded is done.
             "notes": {
                 "value": "This is a test tray seeding",
@@ -217,7 +385,86 @@ describe('Testing if logs are properly removed from the database', () => {
                 },
             ],
             "created": dayjs().unix(),
-            "lot_number": "N/A (No Variety)",
+            "lot_number": "CABBAGE",
+            "data": "{\"crop_tid\": \"161\"}"
+        }       
+
+        return createRecord('/log', json, sessionToken)
+    }
+
+    function makeTraySeeding(name) {
+        let json = {
+            "name": name,
+            "type": "farm_seeding",
+            "timestamp": dayjs("2023-05-02").unix(),
+            "done": "1",  //any seeding recorded is done.
+            "notes": {
+                "value": "This is a test tray seeding",
+                "format": "farm_format"
+            },
+            "asset": [{ 
+                "id": "6",   //Associated planting
+                "resource": "farm_asset"
+            }],
+            "log_category": [{
+                "id": "241",
+                "resource": "taxonomy_term"
+            }],
+            "movement": {
+                "area": [{
+                    "id": "180",
+                    "resource": "taxonomy_term"
+                }]
+            },
+            "quantity": [
+                {
+                    "measure": "count", 
+                    "value": "10",  //number of seed planted
+                    "unit": {
+                        "id": "17", 
+                        "resource": "taxonomy_term"
+                    },
+                    "label": "Seeds planted"
+                },
+                {
+                    "measure": "count", 
+                    "value": "20",  //number of flats(trays)
+                    "unit": {
+                        "id": "12", 
+                        "resource": "taxonomy_term"
+                    },
+                    "label": "Flats used"
+                },
+                {
+                    "measure": "ratio", 
+                    "value": "30",  //cells per flat
+                    "unit": {
+                        "id": "37",
+                        "resource": "taxonomy_term"
+                    },
+                    "label": "Cells/Flat"
+                },
+                {
+                    "measure": "time", 
+                    "value": "1.23",  //hours worked
+                    "unit": {
+                        "id": "29",
+                        "resource": "taxonomy_term"
+                    },
+                    "label": "Labor"
+                },
+                {
+                    "measure": "count", 
+                    "value": "40",
+                    "unit": {
+                        "id": "15",
+                        "resource": "taxonomy_term"
+                    },
+                    "label": "Workers"
+                },
+            ],
+            "created": dayjs().unix(),
+            "lot_number": "CELERY",
             "data": "{\"crop_tid\": \"161\"}"
         }       
 
