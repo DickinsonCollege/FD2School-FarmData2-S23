242caee9ac10b3421e69c08e4cb88934170c19d6
saiatl2354 <atluris@dickinson.edu>
2023-05-10 09:24:39 -0400

    Updated dropdown test

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.seeding.type.filter.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.seeding.type.filter.spec.js
@@ -3,8 +3,9 @@ describe('Testing the seeding type filter', () => {
         cy.login("manager1", "farmdata2")
         cy.visit("/farm/fd2-barn-kit/seedingReport")
     })
+    
     it("Tests that direct and tray seedings are both shown when All is selected", () => {
-        //Selects time range
+        //Selects date range
         cy.get('[data-cy=start-date-select]').type('2019-02-13')
         cy.get('[data-cy=end-date-select]').type('2019-02-15')
         cy.get('[data-cy=generate-rpt-btn]').click()
@@ -15,7 +16,6 @@ describe('Testing the seeding type filter', () => {
         cy.get('[data-cy=seeding-type-dropdown] > [data-cy=dropdown-input]').select('Tray Seedings')
         cy.get('[data-cy=tray-summary]').should('exist')
     })
-
 
     /**
      * The test below will check that when the Direct Seeding option is selected,
@@ -42,6 +42,18 @@ describe('Testing the seeding type filter', () => {
         cy.get('[data-cy=direct-summary]').should('not.exist')
     })
 
+    it("Tests that only the seeding types shown in the date range are shown in the dropdown", () => {
+        //Selects date range
+        cy.get('[data-cy=start-date-select]').type('2019-02-13')
+        cy.get('[data-cy=end-date-select]').type('2019-02-15')
+        cy.get('[data-cy=generate-rpt-btn]').click()
+        //Checks whether the dropdown only shows the seeding types in the available data range
+        cy.get('[data-cy=seeding-type-dropdown] > [data-cy=dropdown-input]').should('have.value', 'All')
+        cy.get('[data-cy=seeding-type-dropdown] > [data-cy=dropdown-input]').children
+        cy.get('[data-cy=seeding-type-dropdown]').should('exist')
+        cy.get('[data-cy=seeding-type-dropdown] > [data-cy=dropdown-input]').select('Area')
+        cy.get('[data-cy=area-dropdown]').should('exist')
+    })
 
 })
 
