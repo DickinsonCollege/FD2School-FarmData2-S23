64840749f9bd72c03f59866ff5126be1941d03f1
maliham31 <maliham@dickinson.edu>
2023-06-03 16:21:54 -0400

    updated spec file

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -15,4 +15,15 @@ describe("Test the harvest report default values", () => {
         cy.get("[data-cy=End-date]")
             .should("have.value", "2020-05-15")
     })
+
+    it("check if crop select is correct", () => {
+        cy.get("[data-cy=select-crop]").children().eq(0)
+            .should("have.value", "ARUGULA")
+        cy.get("[data-cy=select-crop]").children().eq(4)
+            .should("have.value", "BEAN-FAVA")
+        cy.get("[data-cy=select-crop]").children().eq(110)  
+            .should("have.value", "ZUCCHINI")
+        cy.get("[data-cy=select-crop]").children()
+            .should("have.length", 111)
+    })
 })
