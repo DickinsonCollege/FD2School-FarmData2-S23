0101c4991a69549f099c0f12fd24320190998ac7
maliham31 <maliham@dickinson.edu>
2023-06-03 22:54:09 -0400

    added fd2 tab

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.defaults.spec.js
@@ -0,0 +1,29 @@
+describe("Test the harvest report default values", () => {
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-school/e2e")
+    })
+      
+    it("Check the page header", () => {
+        cy.get("[data-cy=page-header]")
+            .should("have.text","Harvest Report")
+    })
+
+    it("Check if start and end dates are correct", () => {
+        cy.get("[data-cy=Start-date]")
+            .should("have.value", "2020-05-05")
+        cy.get("[data-cy=End-date]")
+            .should("have.value", "2020-05-15")
+    })
+
+    it("check if crop select is correct", () => {
+        cy.get("[data-cy=select-crop]").children().eq(0)
+            .should("have.value", "ARUGULA")
+        cy.get("[data-cy=select-crop]").children().eq(4)
+            .should("have.value", "BEAN-FAVA")
+        cy.get("[data-cy=select-crop]").children().eq(110)  
+            .should("have.value", "ZUCCHINI")
+        cy.get("[data-cy=select-crop]").children()
+            .should("have.length", 111)
+    })
+})
