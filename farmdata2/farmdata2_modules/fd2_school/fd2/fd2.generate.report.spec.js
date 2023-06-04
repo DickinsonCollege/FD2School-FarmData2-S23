3879d09ba223c67e536ca5f9fbd8393593a6396b
maliham31 <maliham@dickinson.edu>
2023-06-04 12:58:01 -0400

    added table spec test

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.generate.report.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.generate.report.spec.js
@@ -2,7 +2,7 @@ describe("Test the harvest report User Inputs", () => {
     
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
-        cy.visit("/farm/fd2-school/e2e")
+        cy.visit("/farm/fd2-school/fd2")
     })
   
     it("Check generate button - harvest report title",()=>{
