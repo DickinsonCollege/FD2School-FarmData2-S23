7edddcfaee66d27e2e3cfffd81adbbffe953d306
maliham31 <maliham@dickinson.edu>
2023-06-03 20:53:27 -0400

    fixed cypress test

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.generate.report.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.generate.report.spec.js
@@ -5,20 +5,22 @@ describe("Test the harvest report User Inputs", () => {
         cy.visit("/farm/fd2-school/e2e")
     })
   
-    it("Check the Generate Report Button", () =>{ 
+    it("Check generate button - harvest report title",()=>{
         cy.get("[data-cy=report-header]")
             .should("not.exist")
         cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=report-header]")
+            .should("be.visible")
     })
 
     it("Check generate button - farm info",()=>{
         
-        cy.get("[data-cy=generate-button]").click()
+        cy.get("[data-cy=generate-report-button]").click()
         cy.get("[data-cy=farm-name]")
-            .should("have.text","Sample Farm")
-        cy.get("[data-cy=farm-user]")
-            .should("have.text","manager1")
-        cy.get("[data-cy=farm-language]")
-            .should("have.text","English")
+            .should("have.text"," Sample Farm")
+        cy.get("[data-cy=user-name]")
+            .should("have.text"," manager1")
+        cy.get("[data-cy=language]")
+            .should("have.text"," English")
     })
 })
